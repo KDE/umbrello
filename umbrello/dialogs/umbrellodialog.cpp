221567b689dde9f06cfead9aaef26f9c8614d94e
Luis De la Parra Blum <lparrab@gmx.net>
2003-03-13 00:03:48 +0000

    A Container for DialogPages
    svn path=/trunk/kdesdk/umbrello/; revision=213513

---
+++ umbrello/dialogs/umbrellodialog.cpp
@@ -0,0 +1,33 @@
+
+
+#include "umbrellodialog.h"
+#include "dialogpage.h"
+
+#include <qpoint.h>
+#include <qvbox.h>
+#include <kiconloader.h>
+
+
+UmbrelloDialog::UmbrelloDialog( QWidget *parent, const char *name , bool modal, const QString &caption):
+		KDialogBase( IconList, caption, Ok | Apply | Cancel | Help, Ok , parent, name, modal, true )
+{
+	connect(this,SIGNAL(finished()),this,SLOT(deleteLater()));
+}
+
+UmbrelloDialog::~UmbrelloDialog()
+{
+}
+
+void UmbrelloDialog::addPage( DialogPage *page, const QString &title, const QPixmap &pixmap )
+{
+	page->setAutoApply(false);
+	connect(this,SIGNAL(cancelClicked()),page,SLOT(cancel()));
+	connect(this,SIGNAL(applyClicked()),page,SLOT(apply()));
+	connect(this,SIGNAL(okClicked()),page,SLOT(apply()));
+	
+	QWidget *box = KDialogBase::addVBoxPage(title,title, pixmap.isNull()? DesktopIcon( "misc"): pixmap );
+	page->reparent(box,0,QPoint(0,0));
+}
+
+//#include "umbrellodialog.moc"
+
---
+++ umbrello/dialogs/umbrellodialog.h
@@ -0,0 +1,24 @@
+
+
+
+
+#ifndef UMBRELLO_DIALOG
+#define UMBRELLO_DIALOG
+
+#include <kdialogbase.h>
+#include <qpixmap.h> 
+
+class DialogPage;
+
+class UmbrelloDialog : public KDialogBase
+{
+public:
+	UmbrelloDialog( QWidget *parent, const char *name = 0, bool modal = true, 
+	                const QString &caption = "Umbrello UML Modeller");
+	
+	virtual ~UmbrelloDialog();
+	
+	virtual void addPage( DialogPage *page, const QString &title, const QPixmap &pixmap = QPixmap() );
+};
+
+#endif
