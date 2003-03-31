608af0f68350ab01674852def46ca9e5cb790fd1
Luis De la Parra Blum <lparrab@gmx.net>
2003-03-31 23:05:34 +0000

    -Added documentation -implemented raise/low/to background / to foreground
    svn path=/trunk/kdesdk/umbrello/; revision=217455

--- umbrello/diagram/diagramelement.cpp
+++ umbrello/diagram/diagramelement.cpp
@@ -1,4 +1,9 @@
 /***************************************************************************
+                               diagramelement.cpp
+                             -------------------
+    copyright            : (C) 2003 Luis De la Parra
+ ***************************************************************************/
+/***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -9,6 +14,8 @@
 
 #include "diagramelement.h"
 #include "diagram.h"
+#include <kiconloader.h>
+#include <klocale.h>
 #include <qpoint.h>
 #include <qpopupmenu.h>
 
@@ -16,13 +23,17 @@
 namespace Umbrello{
 
 DiagramElement::DiagramElement( Diagram *diagram, int id): 
-	QCanvasPolygonalItem(diagram),m_id(id)
+	QCanvasPolygonalItem(diagram),m_id(id), m_useOwnPen(false), m_useOwnBrush(false)
 {
+	setZ(100);  // default depth
+	setPen(this->diagram()->pen());
+	setBrush(this->diagram()->brush());
 }
 	
 DiagramElement::~DiagramElement()
 {
 	hide();
+	canvas()->update();
 }
 	
 int DiagramElement::getID() const
@@ -33,9 +44,8 @@ int DiagramElement::getID() const
 void DiagramElement::moveAbs( int x, int y )
 {
 	QCanvasItem::move(x,y);
-//do we need to do something else? should this be virtual?	
-update();
-canvas()->update();
+	update();
+	canvas()->update();
 	emit moved();
 }
 	
@@ -46,37 +56,135 @@ void DiagramElement::moveAbs( const QPoint &point )
 	
 void DiagramElement::moveBy( int dx, int dy)
 {
-QCanvasPolygonalItem::moveBy(dx,dy);
-//do we need to do something else? should this be virtual?
-
-update();
-canvas()->update();
-	emit moved();	
+	QCanvasPolygonalItem::moveBy(dx,dy);
+	update();
+	canvas()->update();
+	emit moved();
 }
 
 void DiagramElement::fillContextMenu(QPopupMenu &menu)
 {
-	menu.insertItem("properties",this,SLOT(editProperties()));
-	menu.insertItem("to Forground",this,SLOT(moveToForeground()));
-	menu.insertItem("to background",this,SLOT(moveToBackground()));
-	menu.insertItem("delete",this,SLOT(deleteLater()));
+	QPopupMenu *submenu = new QPopupMenu( &menu, "diagramelement submenu");
+	submenu->insertItem(SmallIcon("foreground"),i18n("To forground"),this,SLOT(moveToForeground()));
+	submenu->insertItem(SmallIcon("raise"),i18n("Raise"),this,SLOT(raise()));
+	submenu->insertItem(SmallIcon("lower"),i18n("Lower"),this,SLOT(lower()));
+	submenu->insertItem(SmallIcon("backround"),i18n("To background"),this,SLOT(moveToBackground()));
+	menu.insertItem(i18n("Depth..."),submenu);
+	
+	menu.insertItem(SmallIcon("info"),i18n("Properties"),this,SLOT(editProperties()));
+	menu.insertSeparator( );
+	menu.insertItem(SmallIcon("editdelete"),i18n("Delete"),this,SLOT(deleteLater()));
 }
 
 
 void DiagramElement::moveToForeground()
 {
-	kdDebug()<<"foreground.."<<endl;
 	QCanvasItemList l = collisions(false);
-	kdDebug()<<"list is empty? "<<l.empty();
-//	QCanvasItemList::iterator it;
-//	for(it = collisions(false).begin(); it != collisions(false).end(); ++it)
-//	kdDebug()<<"z = "<<(*it)->z()<<endl;
-	//setZ(1000);
+	if(l.empty()) return; // nothing to do
+	
+	double maxZ = z();
+	QCanvasItemList::iterator it;
+	for( it = l.begin() ; it != l.end() ; ++it )
+	{
+		maxZ = max(maxZ, (*it)->z());
+	}
+	kdDebug()<<"max z is "<<maxZ<<endl;
+	if( z() < maxZ )
+	{
+		setZ( maxZ + 1 );
+	}
+	else // we are already higher or equal than all others,
+	{   //  move all others one down to make sure we are drawn on top
+		for( it = l.begin() ; it != l.end() ; ++it )
+		{
+			(*it)->setZ((*it)->z() - 1 );
+			kdDebug()<<"lowering other"<<maxZ<<endl;
+		}
+	}
 }
+
 void DiagramElement::moveToBackground()
 {
-kdDebug()<<"background..."<<endl;
-setZ(0);
+	QCanvasItemList l = collisions(false);
+	if(l.empty()) return; // nothing to do
+	
+	double minZ = z();
+	QCanvasItemList::iterator it;
+	for( it = l.begin() ; it != l.end() ; ++it )
+	{
+		minZ = min(minZ, (*it)->z());
+	}
+	kdDebug()<<"min z is "<<minZ<<endl;
+	if( z() > minZ )
+	{
+		setZ( minZ - 1 );
+	}
+	else // we are already lower or equal than all others,
+	{   //  move all others one up to make sure we are drawn in the backround
+		for( it = l.begin() ; it != l.end() ; ++it )
+		{
+			(*it)->setZ((*it)->z() + 1 );
+			kdDebug()<<"raising other"<<minZ<<endl;
+		}
+	}
+}
+
+void DiagramElement::raise()
+{
+	QCanvasItemList l = collisions(false);
+	if(l.empty()) return; // nothing to do
+	
+	double next = z();
+	QCanvasItemList::iterator it;
+	for( it = l.begin() ; it != l.end() ; ++it )
+	{
+		if((*it)->z() > z() )
+		{
+			next = min(next, (*it)->z());
+		}
+	}
+	kdDebug()<<"next z is "<<next<<endl;
+	if( z() < next )
+	{
+		setZ( next + 1 );
+	}
+	else // we are already higher or equal than all others,
+	{   //  move all others one down to make sure we are drawn on the top
+		for( it = l.begin() ; it != l.end() ; ++it )
+		{
+			(*it)->setZ((*it)->z() - 1 );
+			kdDebug()<<"lowering other"<<endl;
+		}
+	}
+}
+
+void DiagramElement::lower()
+{
+	QCanvasItemList l = collisions(false);
+	if(l.empty()) return; // nothing to do
+	
+	double prev = z();
+	QCanvasItemList::iterator it;
+	for( it = l.begin() ; it != l.end() ; ++it )
+	{
+		if((*it)->z() < z() )
+		{
+			prev = max(prev, (*it)->z());
+		}
+	}
+	kdDebug()<<"prev z is "<<prev<<endl;
+	if( z() < prev )
+	{
+		setZ( prev - 1 );
+	}
+	else // we are already lower or equal than all others,
+	{   //  move all others one up to make sure we are drawn on the bottom
+		for( it = l.begin() ; it != l.end() ; ++it )
+		{
+			(*it)->setZ((*it)->z() + 1 );
+			kdDebug()<<"raising other"<<endl;
+		}
+	}
 }
 
 void DiagramElement::setSelected(bool sel)
@@ -86,15 +194,18 @@ void DiagramElement::setSelected(bool sel)
 	canvas()->update();
 }
 
-void DiagramElement::doubleClick()
+void DiagramElement::execDefaultAction()
 {
-kdDebug()<<"execute default action."<<endl;
+	kdWarning()<<"DiagramElement::execDefaultAction() called."
+		<<"This should reimplemented by the derived classes"<<endl;
 }
 
 void DiagramElement::editProperties()
 {
-	kdDebug()<<"show Properties dialog"<<endl;
+	kdWarning()<<"DiagramElement::editProperties() called."
+		<<"This should reimplemented by the derived classes"<<endl;
 }
+
 Diagram* DiagramElement::diagram() const
 {
 	return dynamic_cast<Diagram*>(canvas());
--- umbrello/diagram/diagramelement.h
+++ umbrello/diagram/diagramelement.h
@@ -1,4 +1,9 @@
 /***************************************************************************
+                               diagramelement.h
+                             -------------------
+    copyright            : (C) 2003 Luis De la Parra
+ ***************************************************************************/
+/***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -11,61 +16,89 @@
 #define DIAGRAMELEMENT_H
 
 #include <qobject.h>
-#include <qstring.h>
 #include <qpoint.h>
 #include <kdebug.h>
 
-
 #include "diagram.h"
+
 class QPopupMenu;
 
 namespace Umbrello{
 
-
+/** @short Base class for all elements shown in a Diagram */
 class DiagramElement :  public QObject, public QCanvasPolygonalItem 
 {
 	Q_OBJECT
 
 public:
-
-	DiagramElement( Diagram *diagram, int id);
-	
+	/** Constructor
+	* @param diagram The diagram this element should go in
+	* @param id      Unique ID number
+	*/
+	DiagramElement( Diagram *diagram, int id );
+	/** Destructor */
 	virtual ~DiagramElement();
 	
+	/** Return the ID of this element */
 	int getID() const;
 	
+	/** Move the element to an absolute position in the diagram
+	* @param x X coordinate to move to 
+	* @param y Y coordinate to move to
+	*/
 	virtual void moveAbs( int x, int y );
 	
+	/** @see moveAbs( int x, int y ) */
 	virtual void moveAbs( const QPoint & );
 	
+	/** Move the element a certain distance in the diagram, relative to
+	* its current position
+	* @param dx Distance to move in the x axis
+	* @param dy Distance to move in the y axis
+	*/
 	virtual void moveBy( int dx, int dy);
 	
+	/** Populate a context menu with items / actions for this element
+	*  @param menu The popup menu to insert the actions in
+	*/
 	virtual void fillContextMenu(QPopupMenu &menu);
 
+	/** Sets the selected status of the element*/
 	virtual void setSelected(bool);
 	
+	/** Execute the default action for this element. This action is normaly one
+	* of the actions from fillContextMenu, or maybe nothing.
+	* You can call this method when the user doubleclicks on the element, or when he presses
+	* Enter while the element is selected (for example )
+	*/
+	virtual void execDefaultAction();
 	
-	/** Execute some action when double click...
-	some subclasses may choose to call properties, other to do
-	other things*/
-	virtual void doubleClick();
-	
+	/** Return a reference to the diagram in which the element is */
 	Diagram* diagram() const;
 	
 public slots:
+	/** Move the element one level up in the element stack */
+	void raise( );
+	/** Move the element to the forground */
 	void moveToForeground();
+	/** Move the element one level down in the widget stack */
+	void lower( );
+	/** Move the element to the background*/
 	void moveToBackground();
+	
+	/** Show a dialog to edit the properties of the element */
 	virtual void editProperties();
 	
-
 signals:
+	/** Signals that the element has been moved */
 	void moved();
 	
 protected:
 	virtual void drawShape(QPainter& ) = 0;
 
 	int m_id;
-	
+	bool m_useOwnPen;
+	bool m_useOwnBrush;
 };
 
 
