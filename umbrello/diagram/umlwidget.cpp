a1aa3efafca5275823b1c80b488f3612d1ccf9bc
Luis De la Parra Blum <lparrab@gmx.net>
2003-03-31 23:02:46 +0000

    added documentation. small fixes.
    svn path=/trunk/kdesdk/umbrello/; revision=217453

--- umbrello/diagram/umlwidget.cpp
+++ umbrello/diagram/umlwidget.cpp
@@ -1,12 +1,25 @@
-
+ /***************************************************************************
+                               umlwidget.cpp
+                             -------------------
+    copyright            : (C) 2003 Luis De la Parra
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "umlwidget.h"
+#include "associationwidget.h"
+#include "../umlobject.h"
+
+
 #include <qpointarray.h>
 #include <kdebug.h>
-#include "../dialogs/classpropdlg.h"
 
-#include "../umlobject.h"
-//FIXME#include "associationwidget.h"
 
 namespace Umbrello{
 
@@ -15,13 +28,13 @@ UMLWidget::UMLWidget( Diagram *diagram, uint id, UMLObject *object) :
 {
 	connect(object, SIGNAL(modified()),this, SLOT(umlObjectModified()));
 	connect(object, SIGNAL(destroyed()),this, SLOT(deleteLater()));
-}	
+}
 
 UMLWidget::~UMLWidget()
 {
 	hide();
+	canvas()->update();
 }
-
 
 
 QPointArray UMLWidget::areaPoints() const
@@ -35,40 +48,31 @@ QPointArray UMLWidget::areaPoints() const
     pa[2] = pa[1] + QPoint(0, m_height + pw*2);
     pa[3] = pa[0] + QPoint(0, m_height + pw*2);
     return pa;
-}	
+}
 
 void UMLWidget::umlObjectModified()
 {
-//update
-//FIXME
-//first update ourselves (width and height)
-//then make the canvas make us repaint ourselves in the next update
-	//canvas()->setChanged(this->boundingRect());
-	//changeChunks();
-kdDebug()<<"uml object modified!, update me!!!"<<endl;
+	kdWarning()<<"UMLWidget::umlObjectModified() called! "<<endl
+		<<"This method should be reimplemented in subclassed to update the widget"<<endl;
 }
 
+void UMLWidget::execDefaultAction()
+{
+	//call virtual editProperties
+	kdDebug()<<"UMLWidget::execDefaultAction() called"<<endl;
+	editProperties();
+}
 
 //FIXME
 void UMLWidget::registerAssociation(AssociationWidget *a)
 {
-	//m_associations.append(a);
+	m_associations.append(a);
 }
 
-/*void UMLWidget::editProperties( )
+
+QPoint UMLWidget::getConnectingPoint( AssociationWidget *a, const QPoint &from)
 {
-kdDebug()<<"widget propeties"<<endl;
-	ClassPropDlg *dlg = new ClassPropDlg(0L, m_umlObject, ClassPropDlg::page_gen, true);
-
-	if(dlg->exec()) {
-	}
-	dlg -> close(true);//wipe from memory
-	return;
-
-
-}*/
-void UMLWidget::getConnectingPoint(QPoint &conn1, QPoint &conn2, const QPoint &from)
-{
+kdDebug()<<"UMLWidget::getConnectingPoint(QPoint &conn1, QPoint &conn2, const QPoint &from) not implemented"<<endl;
 /*	//for now just allow the assoc. to connect in the direction its comming
 	if(from.y() > y())
 	{
--- umbrello/diagram/umlwidget.h
+++ umbrello/diagram/umlwidget.h
@@ -1,9 +1,22 @@
-
-#ifndef DIA_UMLWIDGET_H
-#define DIA_UMLWIDGET_H
-
+ /***************************************************************************
+                               umlwidget.h
+                             -------------------
+    copyright            : (C) 2003 Luis De la Parra
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ 
+#ifndef UMBRELLO_UMLWIDGET_H
+#define UMBRELLO_UMLWIDGET_H
 
 #include "diagramelement.h"
+#include <qlist.h>
 
 class UMLObject;
 
@@ -11,43 +24,55 @@ namespace Umbrello{
 class Diagram;
 class AssociationWidget;
 
-
+/** Base class for all widgets that represent some kind of UML Object */
 class UMLWidget : public DiagramElement
 {
 Q_OBJECT
 
 public:
+	/** Constructor
+	* @param diagram The diagram this widget should go in
+	* @param id The unique ID of the widget
+	* @object   The UML Object the widget represents
+	*/
 	UMLWidget( Diagram *diagram, uint id, UMLObject *object);
-	
+	/** Destructor */
 	virtual ~UMLWidget();
-	
+	/** Return the UML Object the widget represents */
 	inline UMLObject* umlObject( ) const;
-	
+	/** Return the width of the widget */
 	inline uint width() const;
-	
+	/** Return the height of the widget*/
 	inline uint height() const;
 	
+	/** By default all UMLWidgets' default action is to call editProperties.
+	* Subclasses can override  this behavior*/
+	virtual void execDefaultAction();
+	
+	/** Return the points bounding the widget. This is used to calculate the bounding rectanle*/
 	virtual QPointArray areaPoints() const;
 	
 //<FIXME FIXME FIXME>
+	/** Register an AssociationWidget, so that the widget can calculate the optimal "connecting point"*/
 	virtual void registerAssociation(AssociationWidget *a);
-	virtual void getConnectingPoint(QPoint &conn1, QPoint &conn2, const QPoint &from);
+	/** */
+	virtual QPoint getConnectingPoint( AssociationWidget *a, const QPoint &from);
 //</FIXME>
 
-	
 public slots:
+	/** Inform the widget that the UML Object it is representing has been modified, so that the widget
+	* can update itself */
 	virtual void umlObjectModified();
 	
-//	virtual void editProperties();
-	
 protected:
+	/** (Re)implement this function to draw the widget */
 	virtual void drawShape(QPainter &) = 0;
 	
 	
 	UMLObject *m_umlObject;
 	uint m_width, m_height;
 	
-//	QPtrList<AssociationWidget> m_associations;
+	QPtrList<AssociationWidget> m_associations;
 
 };
 
@@ -57,5 +82,7 @@ uint UMLWidget::width() const  { return m_width;  }
 uint UMLWidget::height() const { return m_height; }
 
 } //end of namespace Umbrello
-#endif  // DIA_UMLWIDGET_H
+
+#endif  // UMBRELLO_UMLWIDGET_H
+
 
