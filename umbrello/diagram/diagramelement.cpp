83d7bb258e36654850ddaee865aacf7d326807c2
Luis De la Parra Blum <lparrab@gmx.net>
2003-04-01 20:00:22 +0000

    min and max function were not found in gcc 3.x fixed.
    svn path=/trunk/kdesdk/umbrello/; revision=217638

--- umbrello/diagram/diagramelement.cpp
+++ umbrello/diagram/diagramelement.cpp
@@ -19,6 +19,12 @@
 #include <qpoint.h>
 #include <qpopupmenu.h>
 
+//anonymous namespace for file-only data
+namespace {
+float min( float a, float b ) { return a < b ? a: b;}
+float max( float a, float b ) { return a > b ? a: b;}
+}
+
 
 namespace Umbrello{
 
