a09f1a921fbd1390073c34cb70d20e6e10773c93
Oliver Kellogg <okellogg@users.sourceforge.net>
2003-08-04 10:13:48 +0000

    Split out from umlviewlist.h
    svn path=/trunk/kdesdk/umbrello/; revision=240903

---
+++ umbrello/umlviewdatalist.h
@@ -0,0 +1,19 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef UMLVIEWDATALIST_H
+#define UMLVIEWDATALIST_H
+
+#include "umlviewdata.h"
+#include <qptrlist.h>
+
+typedef QPtrList<UMLViewData> UMLViewDataList;
+typedef QPtrListIterator<UMLViewData> UMLViewDataListIt;
+
+#endif
