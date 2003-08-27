eb35cfeafa9f7893e80ddf664b43920d674c8098
Jonathan Riddell <jr@jriddell.org>
2003-08-27 18:50:38 +0000

    Whoops, forgot to add these in the last commit.
    svn path=/trunk/kdesdk/umbrello/; revision=245683

---
+++ umbrello/classifierlistitem.cpp
@@ -0,0 +1,27 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <kdebug.h>
+#include <klocale.h>
+
+#include "classifierlistitem.h"
+
+UMLClassifierListItem::UMLClassifierListItem(QObject* parent, QString Name, int id) : UMLObject(parent,Name, id) {
+
+}
+
+UMLClassifierListItem::UMLClassifierListItem(QObject* parent) : UMLObject(parent) {
+
+}
+
+QString UMLClassifierListItem::getShortName() {
+	return getName();
+}
+
+#include "classifierlistitem.moc"
---
+++ umbrello/classifierlistitem.h
@@ -0,0 +1,67 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef CLASSIFIERLISTITEM_H
+#define CLASSIFIERLISTITEM_H
+
+#include <qptrlist.h>
+#include "umlobject.h"
+
+/**
+ *	Classifiers (classes, interfaces) have lists of operations,
+ *	attributes, templates and others.  This is a base class for
+ *	the items in this list.  This abstraction should remove
+ *	duplication of dialogues and allow for stereotypes in lists.
+ *
+ *	@short	A base class for classifier list items (e.g. attributes)
+ *	@author Jonathan Riddell
+ * Bugs and comments to uml-devel@lists.sf.net or http://bugs.kde.org
+ */
+
+class UMLClassifierListItem : public UMLObject {
+	Q_OBJECT
+public:
+	/**
+	 *	Constructor.  Empty.
+	 *
+	 *	@param	parent		The parent to this operation.
+	 *	@param	name		The name of the operation.
+	 *	@param	id		The id of the operation.
+	 */
+	UMLClassifierListItem(QObject* parent, QString Name, int id);
+
+	/**
+	 *	Constructor.  Empty.
+	 *
+	 *	@param	parent		The parent to this operation.
+	 */
+	UMLClassifierListItem(QObject* parent);
+
+	/**
+	 *	Returns a string representation of the operation.
+	 *
+	 *	@param	sig	What type of operation string to show.
+	 *	@return	The string representation of the operation.
+	 */
+	virtual QString toString(Signature_Type sig = st_NoSig) = 0;
+
+	/**
+	 * Display the properties configuration dialogue for the list
+	 * item.
+	 */
+	virtual bool showPropertiesDialogue(QWidget* parent) = 0;
+
+	/**
+	 * Return a short name, usually just getName();
+	 */
+	virtual QString getShortName();
+
+};
+
+#endif
