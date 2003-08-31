5a442c4744e17ad1288aa6ca109c414143fc6252
Brian Thomas <thomas@mail630.gsfc.nasa.gov>
2003-08-31 21:50:20 +0000

    new codegeneration settings page
    svn path=/trunk/kdesdk/umbrello/; revision=247359

---
+++ umbrello/dialogs/codegenerationpolicypage.cpp
@@ -0,0 +1,42 @@
+/***************************************************************************
+                          codegenerationpolicypage.cpp  -  description
+                             -------------------
+    begin                : Tue Jul 29 2003
+    copyright            : (C) 2003 by Brian Thomas
+    email                : brian.thomas@gsfc.nasa.gov
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <klocale.h>
+#include <qlabel.h>
+#include "codegenerationpolicypage.h"
+#include "../codegenerationpolicy.h"
+
+/** This is the page which comes up IF there is no special options for the 
+ * code generator.
+ */
+CodeGenerationPolicyPage::CodeGenerationPolicyPage( QWidget *parent, const char *name, CodeGenerationPolicy * policy )
+	:CodeGenerationPolicyBase(parent,name) 
+{
+	m_parentPolicy = policy;
+}
+
+CodeGenerationPolicyPage::~CodeGenerationPolicyPage()
+{
+	this->disconnect();
+}
+
+void CodeGenerationPolicyPage::apply() { 
+    // do nothing in vanilla version 
+}
+
+void CodeGenerationPolicyPage::setDefaults() { }
+
---
+++ umbrello/dialogs/codegenerationpolicypage.h
@@ -0,0 +1,53 @@
+/***************************************************************************
+                          codegenerationpolicypage.h  -  description
+                             -------------------
+    begin                : Tue Jul 29 2003
+    copyright            : (C) 2003 by Brian Thomas
+    email                : brian.thomas@gsfc.nasa.gov
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef CODEGENERATIONPOLICYPAGE_H
+#define CODEGENERATIONPOLICYPAGE_H
+
+#include <qwidget.h>
+#include "codegenerationpolicybase.h"
+
+class CodeGenerationPolicy;
+
+/**
+ * @author Brian Thomas
+ */
+
+class CodeGenerationPolicyPage : public CodeGenerationPolicyBase  {
+	Q_OBJECT
+public:
+	CodeGenerationPolicyPage (QWidget *parent=0, const char *name=0, CodeGenerationPolicy * policy = 0);
+
+	virtual ~CodeGenerationPolicyPage();
+
+	void setDefaults();
+
+protected:
+
+	CodeGenerationPolicy * m_parentPolicy;
+
+private:
+
+public slots:
+
+	virtual void apply();
+
+
+};
+
+#endif
+
