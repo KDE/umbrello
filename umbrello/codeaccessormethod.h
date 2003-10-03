126713c2b356da05930a9ac32b311c34ff31a6e0
Brian Thomas <thomas@mail630.gsfc.nasa.gov>
2003-10-03 20:52:48 +0000

    no need for loadFromXMI and saveToXMI to be pure virtual. In most cases the sub-class for any given language wont need to make its own method
    svn path=/trunk/kdesdk/umbrello/; revision=256009

--- umbrello/codeaccessormethod.h
+++ umbrello/codeaccessormethod.h
@@ -78,12 +78,12 @@ public:
          * Save the XMI representation of this object
          * @return      bool    status of save
          */
-        virtual bool saveToXMI ( QDomDocument & doc, QDomElement & root ) = 0;
+        virtual bool saveToXMI ( QDomDocument & doc, QDomElement & root );
 
         /**
          * load params from the appropriate XMI element node.
          */
-        virtual void loadFromXMI ( QDomElement & root ) = 0;
+        virtual void loadFromXMI ( QDomElement & root );
 
         /** set the class attributes from a passed object
          */
