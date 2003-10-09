753af6d172b3882c309c05f15456875a444fd302
John Ratke <jratke@comcast.net>
2003-10-09 02:25:36 +0000

    CCMAIL: 65407-done@bugs.kde.org
    Remove setting cursor back to arrow in NoteWidget::mouseReleaseEvent
    
    svn path=/trunk/kdesdk/umbrello/; revision=257379

--- umbrello/notewidget.cpp
+++ umbrello/notewidget.cpp
@@ -127,7 +127,6 @@ bool NoteWidget::activate ( IDChangeLog* ChangeLog /*= 0*/ ) {
 void NoteWidget::mouseReleaseEvent( QMouseEvent * me ) {
 	UMLWidget::mouseReleaseEvent( me );
 	m_bResizing = false;
-	m_pView -> setCursor( KCursor::arrowCursor() );
 }
 
 void NoteWidget::mouseDoubleClickEvent( QMouseEvent * me ) {
