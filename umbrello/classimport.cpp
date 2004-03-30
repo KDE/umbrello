536f67947cb0c25b7b1b5a3c7fc5b2162892f57c
Oliver Kellogg <okellogg@users.sourceforge.net>
2004-03-30 04:59:02 +0000

    importCPP(): Support package nesting.
    svn path=/trunk/kdesdk/umbrello/; revision=299713

--- umbrello/classimport.cpp
+++ umbrello/classimport.cpp
@@ -156,13 +156,17 @@ void ClassImport::importCPP(QStringList headerFileList) {
 
 	for(; it.current();++it ) {
 		CParsedClass* currentParsedClass =  classParser.store.getClassByName(it);
-		QPtrList<CParsedAttribute> *attributes = currentParsedClass->getSortedAttributeList();
-		QPtrListIterator<CParsedAttribute> aIt(*attributes);
 
-		QString pkgName( currentParsedClass->declaredInScope );
+		QStringList parentPkgs = QStringList::split('.', currentParsedClass->declaredInScope);
 		UMLPackage *pkg = NULL;
-		if( ! pkgName.isEmpty() )
-			pkg = (UMLPackage *)createUMLObject(Uml::ot_Package, pkgName);
+		while (! parentPkgs.isEmpty()) {
+			QString pkgName = parentPkgs.front();
+			parentPkgs.pop_front();
+			UMLPackage *parentPkg = pkg;
+			pkg = (UMLPackage *)createUMLObject(Uml::ot_Package, pkgName, "", parentPkg);
+			if (parentPkg)
+				parentPkg->addObject( pkg );
+		}
 		UMLObject *currentClass = createUMLObject(Uml::ot_Class,
 							  currentParsedClass->name,
 							  currentParsedClass->comment,
@@ -170,7 +174,8 @@ void ClassImport::importCPP(QStringList headerFileList) {
 		if (pkg)
 			pkg->addObject( currentClass );
 
-		for( ; aIt.current() ; ++aIt) {
+		QPtrList<CParsedAttribute> *attributes = currentParsedClass->getSortedAttributeList();
+		for (QPtrListIterator<CParsedAttribute> aIt(*attributes); aIt.current() ; ++aIt) {
 			CParsedAttribute *attr = aIt.current();
 			QString scope;
 			Uml::Scope attrScope = Uml::Public;
