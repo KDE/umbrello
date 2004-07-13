fa37f78a5427f1cc9de81df53158bc830c8ae622
Oliver Kellogg <okellogg@users.sourceforge.net>
2004-07-13 05:44:49 +0000

    slotButtonChanged: binner says KDE uses "&OK".
    svn path=/trunk/kdesdk/umbrello/; revision=328557

--- umbrello/aligntoolbar.cpp
+++ umbrello/aligntoolbar.cpp
@@ -248,7 +248,7 @@ void AlignToolBar::slotButtonChanged(int btn)
 	} else {
 		KMessageBox::messageBox(0, KMessageBox::Information,
 			i18n("For alignment you have to select at least 2 objects like classes or actors. You can not align associations."),
-			i18n("Information"), i18n("&Ok"), QString("")
+			i18n("Information"), i18n("&OK"), QString("")
 #if KDE_IS_VERSION(3,2,0)
 			, "showAlignInformation"
 #endif
