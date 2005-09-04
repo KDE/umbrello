8fc4d6159499f4a12392e23defab6a696706ddfc
Oliver Kellogg <okellogg@users.sourceforge.net>
2005-09-04 09:08:59 +0000

    mouseMoveEvent(): Exclude boxes from the solution to bug 106356 because they are transparent. The problem was revealed after fixing bug 112017.
    svn path=/branches/KDE/3.5/kdesdk/umbrello/; revision=456944

--- umbrello/associationwidget.cpp
+++ umbrello/associationwidget.cpp
@@ -2489,7 +2489,7 @@ void AssociationWidget::mouseMoveEvent(QMouseEvent* me) {
     // Prevent the moving vertex from disappearing underneath a widget
     // (else there's no way to get it back.)
     UMLWidget *onW = m_pView->testOnWidget(p);
-    if (onW) {
+    if (onW && onW->getBaseType() != Uml::wt_Box) {  // boxes are transparent
         const int pX = p.x();
         const int pY = p.y();
         const int wX = onW->getX();
