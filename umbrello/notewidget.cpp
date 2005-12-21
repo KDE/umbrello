1cdcf06ffc2942b2e4ce981869be5a6c190d7c1a
Oliver Kellogg <okellogg@users.sourceforge.net>
2005-12-21 20:22:17 +0000

    apply commit 490414 from branches/KDE/3.5
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=490416

--- umbrello/notewidget.cpp
+++ umbrello/notewidget.cpp
@@ -207,6 +207,7 @@ void NoteWidget::slotMenuSelection(int sel) {
 }
 
 void NoteWidget::mouseReleaseEvent( QMouseEvent * me ) {
+    UMLWidget::mouseReleaseEvent( me );
     if (m_bResizing) {
         drawText();
         UMLWidget::mouseReleaseEvent(me);
