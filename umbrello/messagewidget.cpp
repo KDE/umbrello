7dea69f635b81f29676e8d9f60fbe4c4c16a04b5
Oliver Kellogg <okellogg@users.sourceforge.net>
2006-02-13 19:28:27 +0000

    apply commit 509132 from branches/KDE/3.5
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=509135

--- umbrello/messagewidget.cpp
+++ umbrello/messagewidget.cpp
@@ -489,7 +489,11 @@ UMLOperation *MessageWidget::getOperation() {
 }
 
 void MessageWidget::setOperation(UMLOperation *op) {
+    if (m_pObject && m_pFText)
+        disconnect(m_pObject, SIGNAL(modified()), m_pFText, SLOT(setMessageText()));
     m_pObject = op;
+    if (m_pObject && m_pFText)
+        connect(m_pObject, SIGNAL(modified()), m_pFText, SLOT(setMessageText()));
 }
 
 QString MessageWidget::getCustomOpText() {
@@ -820,9 +824,10 @@ bool MessageWidget::loadFromXMI(QDomElement& qElement) {
     updateResizability();
 
     UMLClassifier *c = dynamic_cast<UMLClassifier*>( pWB->getUMLObject() );
+    UMLOperation *op = NULL;
     if (c) {
         Uml::IDType opId = STR2ID(m_CustomOp);
-        UMLOperation *op = dynamic_cast<UMLOperation*>( c->findChildObjectById(opId, true) );
+        op = dynamic_cast<UMLOperation*>( c->findChildObjectById(opId, true) );
         if (op) {
             // If the UMLOperation is set, m_CustomOp isn't used anyway.
             // Just setting it empty for the sake of sanity.
@@ -844,7 +849,6 @@ bool MessageWidget::loadFromXMI(QDomElement& qElement) {
                 m_CustomOp = QString::null;
             }
         }
-        setOperation(op);
     }
 
     Uml::IDType textId = STR2ID(textid);
@@ -883,6 +887,8 @@ bool MessageWidget::loadFromXMI(QDomElement& qElement) {
             << tag << endl;
         }
     }
+    if (op)                // Do it here and not earlier because now we have the
+        setOperation(op);  // m_pFText and setOperation() can make connections.
 
     // always need this
     setLinkAndTextPos();
