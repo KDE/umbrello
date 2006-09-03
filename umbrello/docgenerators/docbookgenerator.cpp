5fd5d77880358a7b83ffe4435d5a7557715523fd
Oliver Kellogg <okellogg@users.sourceforge.net>
2006-09-03 19:20:03 +0000

    missed this in commit 580517
    svn path=/branches/KDE/3.5/kdesdk/umbrello/; revision=580518

--- umbrello/docgenerators/docbookgenerator.cpp
+++ umbrello/docgenerators/docbookgenerator.cpp
@@ -91,7 +91,7 @@ KIO::Job* DocbookGenerator::generateDocbookForProjectInto(const KURL& destDir)
     KMessageBox::error(0, i18n("There was a problem saving file: %1").arg(tmpfile.name()), i18n("Save Error"));
     return false;
   }
-  umlDoc->saveToXMI( file, false ); // save the xmi stuff to it
+  umlDoc->saveToXMI(file); // save the xmi stuff to it
   file.close();
   tmpfile.close();
   
