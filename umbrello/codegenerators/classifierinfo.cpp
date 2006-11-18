e24294905d33e010ab06b6eabf362f253d1e9ad8
Oliver Kellogg <okellogg@users.sourceforge.net>
2006-11-18 18:42:24 +0000

    (C) 2006
    svn path=/branches/KDE/3.5/kdesdk/umbrello/; revision=605963

--- umbrello/codegenerators/classifierinfo.cpp
+++ umbrello/codegenerators/classifierinfo.cpp
@@ -1,14 +1,12 @@
 /***************************************************************************
-    copyright       : (C) 2003 Brian Thomas brian.thomas@gsfc.nasa.gov
-***************************************************************************/
-
-/***************************************************************************
- *                                                                       *
+ *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
- *                                                                       *
+ *                                                                         *
+ *   copyright       : (C) 2003 Brian Thomas brian.thomas@gsfc.nasa.gov    *
+ *   (C) 2004-2006  Umbrello UML Modeller Authors <uml-devel@uml.sf.net>   *
  ***************************************************************************/
 
 
