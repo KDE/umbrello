42a1d345c35209d4da7d5d24f9fe58fbbcce2d29
Thibault Normand <thibault.normand@gmail.com>
2007-01-29 21:31:15 +0000

    # UMLObject U/R
    svn path=/branches/work/isi-umbrello/umbrello/; revision=628363

---
+++ umbrello/cmds/generic/cmd_create_umlobject.h
@@ -0,0 +1,35 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   copyright (C) 2002-2007                                               *
+ *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
+ ***************************************************************************/
+
+#ifndef __CMD_CREATE_UMLOBJECT__
+#define __CMD_CREATE_UMLOBJECT__
+
+#include <QUndoCommand>
+
+#include "umlobject.h"
+
+namespace Uml
+{
+	class cmdCreateUMLObject : public QUndoCommand
+	{
+		public:
+			cmdCreateUMLObject(UMLObject* o);
+			~cmdCreateUMLObject();
+
+			void redo();
+			void undo();
+		
+		private:
+			UMLObject*	m_obj;
+	};
+};
+
+#endif
