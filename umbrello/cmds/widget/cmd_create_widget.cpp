80a068399b5358faf9d7bfeb3c3c84e6f9572e87
Thibault Normand <thibault.normand@gmail.com>
2007-01-29 21:32:02 +0000

    # UMLWidget U/R
    svn path=/branches/work/isi-umbrello/umbrello/; revision=628364

---
+++ umbrello/cmds/widget/cmd_create_widget.cpp
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   copyright (C) 2002-2007                                               *
+ *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
+ ***************************************************************************/
+
+#include "cmd_create_widget.h"
+
+#include "uml.h"
+#include "umldoc.h"
+
+#include <klocale.h>
+
+namespace Uml
+{
+
+	cmdCreateWidget::cmdCreateWidget(UMLView* view, UMLWidget* w):m_view(view), m_widget(w)
+	{
+		setText(i18n("Create widget :") + w->getName());
+	}
+	
+	cmdCreateWidget::~cmdCreateWidget()
+	{
+	}
+	
+	// Create the UMLObject
+	void cmdCreateWidget::redo()
+	{
+		m_view->getWidgetList().append(m_widget);
+	}
+	
+	// Suppress the UMLObject
+	void cmdCreateWidget::undo()
+	{
+		m_view->removeWidget(m_widget);
+	}
+
+}
---
+++ umbrello/cmds/widget/cmd_create_widget.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   copyright (C) 2002-2007                                               *
+ *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
+ ***************************************************************************/
+
+#ifndef __CMD_CREATE_WIDGET__
+#define __CMD_CREATE_WIDGET__
+
+#include <QUndoCommand>
+#include "umlwidget.h"
+#include "umlview.h"
+
+namespace Uml
+{
+	class cmdCreateWidget : public QUndoCommand
+	{
+		public:
+			cmdCreateWidget(UMLView* view, UMLWidget* w);
+			~cmdCreateWidget();
+
+			void redo();
+			void undo();
+		
+		private:
+			UMLWidget*	m_widget;
+			UMLView*	m_view;
+	};
+};
+
+#endif
