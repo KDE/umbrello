5a75ac0b620f236ceccbe5f734f3491a3ffe240e
Oliver Kellogg <okellogg@users.sourceforge.net>
2007-02-19 19:32:50 +0000

    add license
    svn path=/branches/KDE/3.5/kdesdk/umbrello/; revision=635309

--- umbrello/codegenerators/csharpwriter.cpp
+++ umbrello/codegenerators/csharpwriter.cpp
@@ -1,13 +1,16 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   copyright (C) 2007                                                    *
+ *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
+ ***************************************************************************/
+
 //
 // C++ Implementation: csharpwriter
-//
-// Description: 
-//
-//
-// Author: Umbrello UML Modeller Authors <uml-devel@lists.sourceforge.net>, (C) 2007
-//
-// Copyright: See COPYING file that comes with this distribution
-//
 //
 #include "csharpwriter.h"
 
--- umbrello/codegenerators/csharpwriter.h
+++ umbrello/codegenerators/csharpwriter.h
@@ -1,13 +1,18 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   copyright (C) 2007                                                    *
+ *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
+ ***************************************************************************/
+
 //
 // C++ Interface: csharpwriter
 //
-// Description: 
-//
-//
-// Author: Umbrello UML Modeller Authors <uml-devel@lists.sourceforge.net>, (C) 2007
-//
-// Copyright: See COPYING file that comes with this distribution
-//
+// @author Ferenc Veres
 //
 #ifndef CSHARPWRITER_H
 #define CSHARPWRITER_H
