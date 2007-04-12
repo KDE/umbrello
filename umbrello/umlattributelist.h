93a55559e75419dace148260c1bfd35f607d7116
Oliver Kellogg <okellogg@users.sourceforge.net>
2007-04-12 18:35:05 +0000

    apply commit 653147 from trunk
    svn path=/branches/KDE/3.5/kdesdk/umbrello/; revision=653156

--- umbrello/umlattributelist.h
+++ umbrello/umlattributelist.h
@@ -14,8 +14,7 @@
 
 #include <qptrlist.h>
 
-// forward declaration
-class UMLAttribute;
+#include "attribute.h"
 
 //typedef QPtrList<UMLAttribute> UMLAttributeList;
 typedef QPtrListIterator<UMLAttribute> UMLAttributeListIt;
