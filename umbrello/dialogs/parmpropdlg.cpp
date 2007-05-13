6df20d2374eb9aa92044903fd72cdd1de18c26b8
Laurent Montel <montel@kde.org>
2007-05-13 11:25:12 +0000

    Add macro Q_OBJECT so slot can be found
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=664205

--- umbrello/dialogs/parmpropdlg.cpp
+++ umbrello/dialogs/parmpropdlg.cpp
@@ -34,6 +34,8 @@
 #include "../object_factory.h"
 #include "../stereotype.h"
 
+#include "parmpropdlg.moc"
+
 ParmPropDlg::ParmPropDlg(QWidget * parent, UMLDoc * doc, UMLAttribute * a)
         : KDialog(parent)
 {
