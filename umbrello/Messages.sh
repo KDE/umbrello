45284b059abc3f7edbfc21eb64beca78e487520f
Stephan Kulow <coolo@kde.org>
2007-06-10 09:36:33 +0000

    there are no tips
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=673446

--- umbrello/Messages.sh
+++ umbrello/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-$PREPARETIPS > tips.cpp
+#$PREPARETIPS > tips.cpp
 $EXTRACTRC *.rc codegenerators/*.ui dialogs/*.ui > ./rc.cpp || exit 11
 LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
 if test -n "$LIST"; then
