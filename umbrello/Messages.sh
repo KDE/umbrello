d96bef68fdd023afd2a5475f25427d5f66e3697a
Laurent Montel <montel@kde.org>
2007-08-09 20:40:31 +0000

    Don't complexify it
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=698320

--- umbrello/Messages.sh
+++ umbrello/Messages.sh
@@ -1,8 +1,5 @@
 #! /bin/sh
 #$PREPARETIPS > tips.cpp
 $EXTRACTRC *.rc codegenerators/*.ui dialogs/*.ui > ./rc.cpp || exit 11
-LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
-if test -n "$LIST"; then
-$XGETTEXT $LIST rc.cpp -o $podir/umbrello.pot
-fi
+$XGETTEXT *.cpp *.h -o $podir/umbrello.pot
 rm -f tips.cpp
