39deaf74d7bfb46c3b83cc8baa7408f7db2637c4
Burkhard Lück <lueck@hube-lueck.de>
2007-11-04 20:32:48 +0000

    fixed messages target
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=732813

--- umbrello/Messages.sh
+++ umbrello/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 #$PREPARETIPS > tips.cpp
 $EXTRACTRC *.rc codegenerators/*.ui dialogs/*.ui > ./rc.cpp || exit 11
-$XGETTEXT *.cpp *.h -o $podir/umbrello.pot
+$XGETTEXT `find . -name \*.h -o -name \*.cpp` -o $podir/umbrello.pot
 rm -f tips.cpp
