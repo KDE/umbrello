72e28042040479fa94d6611726bdaf3e2ad0db83
Sharan Rao <sharanrao@gmail.com>
2007-11-16 11:47:42 +0000

    modify common multiplicities
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=737499

--- umbrello/dialogs/assocrolepage.cpp
+++ umbrello/dialogs/assocrolepage.cpp
@@ -89,7 +89,7 @@ void AssocRolePage::constructWidget() {
     propsBLayout -> setMargin(margin);
 
     QStringList multiplicities;
-    multiplicities<<"1"<<"*"<<"1..*"<<"*..1";
+    multiplicities<<"1"<<"*"<<"1..*"<<"0..1";
 
     // Properties
     //
