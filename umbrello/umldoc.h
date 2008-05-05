ca2daebdd324ff7e1d1e295538d885e751730ed5
Andi Fischer <andi.fischer@hispeed.ch>
2008-05-05 17:46:40 +0000

    Change to new tab widget with close buttons, fix moving tabs left and right, some code cleanups.
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=804323

--- umbrello/umldoc.h
+++ umbrello/umldoc.h
@@ -5,7 +5,7 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   copyright (C) 2002-2007                                               *
+ *   copyright (C) 2002-2008                                               *
  *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
  ***************************************************************************/
 
@@ -48,22 +48,23 @@ class UMLFolder;
 class DiagramPrintPage;
 
 /**
-  * UMLDoc provides a document object for a document-view model.
-  *
-  * The UMLDoc class provides a document object that can be used
-  * in conjunction with the classes UMLApp and UMLView to create
-  * a document-view model for standard KDE applications based on
-  * KApplication and KMainWindow. Thereby, the document object
-  * is created by the UMLApp instance and contains the document
-  * structure with the according methods for manipulation of the
-  * document data by UMLView objects. Also, UMLDoc contains the
-  * methods for serialization of the document data from and to
-  * files.
-  *
-  * @author Paul Hensgen        <phensgen@techie.com>
-  * Bugs and comments to uml-devel@lists.sf.net or http://bugs.kde.org
-  */
-class UMLDoc : public QObject {
+ * UMLDoc provides a document object for a document-view model.
+ *
+ * The UMLDoc class provides a document object that can be used
+ * in conjunction with the classes UMLApp and UMLView to create
+ * a document-view model for standard KDE applications based on
+ * KApplication and KMainWindow. Thereby, the document object
+ * is created by the UMLApp instance and contains the document
+ * structure with the according methods for manipulation of the
+ * document data by UMLView objects. Also, UMLDoc contains the
+ * methods for serialization of the document data from and to
+ * files.
+ *
+ * @author Paul Hensgen   <phensgen@techie.com>
+ * Bugs and comments to uml-devel@lists.sf.net or http://bugs.kde.org
+ */
+class UMLDoc : public QObject
+{
     Q_OBJECT
 public:
     /**
@@ -86,16 +87,16 @@ public:
      * Adds a view to the document which represents the document
      * contents. Usually this is your main view.
      *
-     * @param view              Pointer to the UMLView to add.
+     * @param view   Pointer to the UMLView to add.
      */
     void addView(UMLView *view);
 
     /**
      * Removes a view from the list of currently connected views.
      *
-     * @param view              Pointer to the UMLView to remove.
-     * @param enforceOneView    switch to determine if we have a current view or not.
-     *                          most of the time, we DO want this, except when exiting the program.
+     * @param view             Pointer to the UMLView to remove.
+     * @param enforceOneView   switch to determine if we have a current view or not.
+     *                         most of the time, we DO want this, except when exiting the program.
      */
     void removeView(UMLView *view , bool enforceOneView = true );
 
@@ -108,7 +109,7 @@ public:
      * Sets the modified flag for the document after a modifying
      * action on the view connected to the document.
      *
-     * @param _m                The value to set the modified flag to.
+     * @param _m   The value to set the modified flag to.
      */
     void setModified(bool _m=true);
 
@@ -147,8 +148,8 @@ public:
      * Loads the document by filename and format and emits the
      * updateViews() signal.
      *
-     * @param url               The filename in KUrl format.
-     * @param format    The format (optional.)
+     * @param url      The filename in KUrl format.
+     * @param format   The format (optional.)
      * @return  True if operation successful.
      */
     bool openDocument(const KUrl& url, const char *format=0);
@@ -156,8 +157,8 @@ public:
     /**
      * Saves the document using the given filename and format.
      *
-     * @param url               The filename in KUrl format.
-     * @param format    The format (optional.)
+     * @param url      The filename in KUrl format.
+     * @param format   The format (optional.)
      * @return  True if operation successful.
      */
     bool saveDocument(const KUrl& url, const char *format=0);
@@ -172,7 +173,7 @@ public:
     /**
      * Sets the URL of the document.
      *
-     * @param url               The KUrl to set.
+     * @param url   The KUrl to set.
      */
     void setUrl(const KUrl& url);
 
@@ -184,7 +185,7 @@ public:
     /**
      * Returns true if the given name is unique within its scope.
      *
-     * @param name              The name to check.
+     * @param name   The name to check.
      * @return  True if name is unique.
      */
     bool isUnique(const QString &name);
@@ -192,8 +193,8 @@ public:
     /**
       * Returns true if the given name is unique within its scope of given package.
       *
-      * @param name          The name to check.
-      * @param package  The UMLPackage in which we have to determine the unique-ness
+      * @param name      The name to check.
+      * @param package   The UMLPackage in which we have to determine the unique-ness
       * @return      True if name is unique.
       */
     bool isUnique(const QString &name, UMLPackage *package);
@@ -207,9 +208,9 @@ public:
      * Creates an association between two UMLObjects.
      * NOTE: this method does not check if the association is valid / legal
      *
-     * @param a The UMLObject "A" for the association (source)
-     * @param b The UMLObject "B" for the association (destination)
-     * @param type      The association's type
+     * @param a      The UMLObject "A" for the association (source)
+     * @param b      The UMLObject "B" for the association (destination)
+     * @param type   The association's type
      * @return  The Association created
      */
     UMLAssociation* createUMLAssociation(UMLObject *a, UMLObject *b, Uml::Association_Type type);
@@ -235,7 +236,7 @@ public:
      * @param assocType Type of the UMLAssociation to seek.
      * @param roleAObj  Pointer to the role A UMLCanvasObject.
      * @param roleBObj  Pointer to the role B UMLCanvasObject.
-     * @param swap              Optional pointer to boolean.
+     * @param swap      Optional pointer to boolean.
      *                  The bool is set to true if the assocation
      *                  matched with swapped roles, else it is set
      *                  to false.
@@ -249,10 +250,10 @@ public:
     /**
      * Creates a diagram of the given type.
      *
-     * @param folder            The folder in which tp create the diagram.
-     * @param type              The type of diagram to create.
-     * @param askForName        If true shows a dialog box asking for name,
-     *                  else uses a default name.
+     * @param folder       The folder in which tp create the diagram.
+     * @param type         The type of diagram to create.
+     * @param askForName   If true shows a dialog box asking for name,
+     *                     else uses a default name.
      * @return Pointer to the UMLView of the new diagram.
      */
     UMLView* createDiagram(UMLFolder *folder, Uml::Diagram_Type type, bool askForName = true);
@@ -262,7 +263,7 @@ public:
      * is being represented on a diagram it will also delete all those
      * representations.
      *
-     * @param o         Pointer to the UMLObject to delete.
+     * @param o   Pointer to the UMLObject to delete.
      */
     void removeUMLObject(UMLObject*o);
 
@@ -270,7 +271,7 @@ public:
      * Used to rename a document.  This method takes care of everything.
      * You just need to give the ID of the diagram to the method.
      *
-     * @param id        The ID of the diagram to rename.
+     * @param id   The ID of the diagram to rename.
      */
     void renameDiagram(Uml::IDType id);
 
@@ -293,21 +294,21 @@ public:
     /**
      * Changes the current view (diagram) to the view with the given ID.
      *
-     * @param id                The ID of the view to change to.
+     * @param id   The ID of the view to change to.
      */
     void  changeCurrentView(Uml::IDType id);
 
     /**
      * Deletes a diagram from the current file.
      *
-     * @param id                The ID of the diagram to delete.
+     * @param id   The ID of the diagram to delete.
      */
     void removeDiagram(Uml::IDType id);
 
     /**
      * Used to find a reference to a @ref UMLObject by its ID.
      *
-     * @param id                The @ref UMLObject to find.
+     * @param id   The @ref UMLObject to find.
      * @return  Pointer to the UMLObject found, or NULL if not found.
      */
     UMLObject* findObjectById(Uml::IDType id);
@@ -315,14 +316,14 @@ public:
     /**
      * Used to find a @ref UMLObject by its type and name.
      *
-     * @param name              The name of the @ref UMLObject to find.
-     * @param type              Object_Type of the object to find (optional.)
-     *                  When the given type is ot_UMLObject the type is
-     *                  disregarded, i.e. the given name is the only
-     *                  search criterion.
-     * @param currentObj        Object relative to which to search (optional.)
-     *                  If given then the enclosing scope(s) of this
-     *                  object are searched before the global scope.
+     * @param name         The name of the @ref UMLObject to find.
+     * @param type         Object_Type of the object to find (optional.)
+     *                     When the given type is ot_UMLObject the type is
+     *                     disregarded, i.e. the given name is the only
+     *                     search criterion.
+     * @param currentObj   Object relative to which to search (optional.)
+     *                     If given then the enclosing scope(s) of this
+     *                     object are searched before the global scope.
      * @return  Pointer to the UMLObject found, or NULL if not found.
      */
     UMLObject* findUMLObject(const QString &name,
@@ -335,7 +336,7 @@ public:
      * Only used for intermediate processing while loading files
      * containing objects with non-numeric xmi.id's.
      *
-     * @param idStr             The AuxId for the @ref UMLObject to find.
+     * @param idStr   The AuxId for the @ref UMLObject to find.
      * @return  Pointer to the UMLObject found, or NULL if not found.
      */
     UMLObject* findObjectByAuxId(const QString &idStr);
@@ -343,14 +344,14 @@ public:
     /**
      * Used to find a @ref UMLClassifier by its name.
      *
-     * @param name              The name of the @ref UMLObject to find.
+     * @param name   The name of the @ref UMLObject to find.
      */
     UMLClassifier * findUMLClassifier (const QString &name);
 
     /**
      * Finds a UMLStereotype by its name.
      *
-     * @param name              The name of the UMLStereotype to find.
+     * @param name   The name of the UMLStereotype to find.
      * @return  Pointer to the UMLStereotype found, or NULL if not found.
      */
     UMLStereotype * findStereotype(const QString &name);
@@ -358,7 +359,7 @@ public:
     /**
      * Finds a view (diagram) by the ID given to method.
      *
-     * @param id                The ID of the view to search for.
+     * @param id   The ID of the view to search for.
      * @return  Pointer to the view found, or NULL if not found.
      */
     UMLView * findView(Uml::IDType id);
@@ -366,8 +367,8 @@ public:
     /**
      * Finds a view (diagram) by the type and name given.
      *
-     * @param type              The type of view to find.
-     * @param name              The name of the view to find.
+     * @param type            The type of view to find.
+     * @param name            The name of the view to find.
      * @param searchAllScopes Search in all subfolders (default: false.)
      * @return  Pointer to the view found, or NULL if not found.
      */
@@ -403,7 +404,7 @@ public:
      * Checks the given XMI file if it was saved with correct Unicode
      * encoding set or not.
      *
-     * @param file              The file to be checked.
+     * @param file   The file to be checked.
      */
     short getEncoding(QIODevice & file);
 
@@ -412,8 +413,8 @@ public:
      * is already known it can be passed to the function. If this info
      * isn't given, loadFromXMI will check which encoding was used.
      *
-     * @param file              The file to be loaded.
-     * @param encode    The encoding used.
+     * @param file     The file to be loaded.
+     * @param encode   The encoding used.
      */
     virtual bool loadFromXMI(QIODevice& file, short encode = ENC_UNKNOWN);
 
@@ -421,7 +422,7 @@ public:
      * Ensures the XMI file is a valid UML file.
      * Currently only checks for metamodel=UML.
      *
-     * @param headerNode        The <XMI.header> node
+     * @param headerNode   The <XMI.header> node
      */
     bool validateXMIHeader(QDomNode& headerNode);
 
@@ -475,8 +476,8 @@ public:
     /**
      * Returns a list of the concepts in this UMLDoc.
      *
-     * @param includeNested             Whether to include the concepts from
-     *                          nested packages (default: true.)
+     * @param includeNested   Whether to include the concepts from
+     *                        nested packages (default: true.)
      * @return  List of UML concepts.
      */
     UMLClassifierList getConcepts(bool includeNested = true);
@@ -484,8 +485,8 @@ public:
     /**
      * Returns a list of the classes in this UMLDoc.
      *
-     * @param includeNested             Whether to include the concepts from
-     *                          nested packages (default: true.)
+     * @param includeNested   Whether to include the concepts from
+     *                        nested packages (default: true.)
      * @return  List of UML classes.
      */
     UMLClassifierList getClasses(bool includeNested = true);
@@ -493,8 +494,8 @@ public:
     /**
      * Returns a list of the classes and interfaces in this UMLDoc.
      *
-     * @param includeNested             Whether to include the concepts from
-     *                          nested packages (default: true.)
+     * @param includeNested   Whether to include the concepts from
+     *                        nested packages (default: true.)
      * @return  List of UML concepts.
      */
     UMLClassifierList getClassesAndInterfaces(bool includeNested = true);
@@ -502,8 +503,8 @@ public:
     /**
      * Returns a list of the entities in this UMLDoc.
      *
-     * @param includeNested     Whether to include the entities from
-     *                          nested packages (default: true.)
+     * @param includeNested   Whether to include the entities from
+     *                        nested packages (default: true.)
      * @return  List of UML Entities.
      */
     UMLEntityList getEntities(bool includeNested = true);
@@ -511,8 +512,8 @@ public:
     /**
      * Returns a list of the interfaces in this UMLDoc.
      *
-     * @param includeNested             Whether to include the concepts from
-     *                          nested packages (default: true.)
+     * @param includeNested   Whether to include the concepts from
+     *                        nested packages (default: true.)
      * @return  List of UML interfaces.
      */
     UMLClassifierList getInterfaces(bool includeNested = true);
@@ -558,10 +559,10 @@ public:
      * If the object is a classifier then the operations/attributes
      * are also assigned new IDs.
      *
-     * @param Obj               Pointer to the UMLObject to add.
+     * @param obj   Pointer to the UMLObject to add.
      * @return  True if operation successful.
      */
-    bool assignNewIDs(UMLObject* Obj);
+    bool assignNewIDs(UMLObject* obj);
 
     /**
      * Adds a UMLObject thats already created but doesn't change
@@ -579,7 +580,7 @@ public:
      * the others; this number is incremental so if number 1 is in
      * use then it tries 2 and then 3 and so on
      *
-     * @param pView             Pointer to the UMLView to add.
+     * @param pView   Pointer to the UMLView to add.
      * @return  True if operation successful.
      */
     bool addUMLView(UMLView * pView );
@@ -634,10 +635,10 @@ public:
      * Assigns a New ID to an Object, and also logs the assignment
      * to its internal ChangeLog.
      *
-     * @param OldID             The present ID of the object.
+     * @param oldID   The present ID of the object.
      * @return  The new ID assigned to the object.
      */
-    Uml::IDType assignNewID(Uml::IDType OldID);
+    Uml::IDType assignNewID(Uml::IDType oldID);
 
     /**
      * Returns the documentation for the project.
