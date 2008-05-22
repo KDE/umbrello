57bf312940dbff92b46f46b9d98970e7f6542f01
Gopala Krishna A <krishna.ggk@gmail.com>
2008-05-22 11:07:56 +0000

    Made umbrello to derive version number from KDE's version , that is use (major-2).minor.release CCMAIL:uml-devel@lists.sourceforge.net
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=811127

---
+++ umbrello/version.h
@@ -0,0 +1,25 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   copyright (C) 2002-2008                                               *
+ *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
+ ***************************************************************************/
+
+#ifndef __UMBRELLO_VERSION_H
+#define __UMBRELLO_VERSION_H
+
+#include <kdeversion.h>
+
+inline QByteArray umbrelloVersion()
+{
+    return QString("%1.%2.%3")
+        .arg(KDE::versionMajor()-2)
+        .arg(KDE::versionMinor())
+        .arg(KDE::versionRelease()).toLatin1();
+}
+
+#endif
