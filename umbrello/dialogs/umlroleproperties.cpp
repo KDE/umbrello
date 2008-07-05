36560d82821778ca8ee983965b033c7ff50d0945
Andi Fischer <andi.fischer@hispeed.ch>
2008-07-05 14:33:03 +0000

    Replacing Q-widgets with K-widgets.
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=828369

--- umbrello/dialogs/umlroleproperties.cpp
+++ umbrello/dialogs/umlroleproperties.cpp
@@ -1,21 +1,22 @@
 /***************************************************************************
- *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   copyright (C) 2003-2007                                               *
+ *   copyright (C) 2003-2008                                               *
  *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
  ***************************************************************************/
 
 // own header
 #include "umlroleproperties.h"
 
-// qt/kde includes
-#include <qradiobutton.h>
-#include <q3textedit.h>
-#include <qlineedit.h>
+// qt includes
+//#include <QtGui/QRadioButton>
+//#include <q3textedit.h>
+//#include <qlineedit.h>
+
+// kde includes
 #include <klocale.h>
 #include <kmessagebox.h>
 #include <kdebug.h>
@@ -24,90 +25,87 @@
 UMLRoleProperties::UMLRoleProperties ( QWidget *parent, UMLRole *role)
         : UMLRolePropertiesBase (parent)
 {
-
     m_pRole = role;
     constructWidget();
-
 }
 
-UMLRoleProperties::~UMLRoleProperties() { }
+UMLRoleProperties::~UMLRoleProperties()
+{
+}
 
-void UMLRoleProperties::constructWidget() {
-
+void UMLRoleProperties::constructWidget()
+{
     // Use Parent Role to set starting Properties
-    //
 
     // Rolename
-    m_pRoleLE -> setText(m_pRole->getName());
+    ui_pRoleLE->setText(m_pRole->getName());
 
     // Multiplicity
-    m_pMultiLE -> setText(m_pRole->getMultiplicity());
+    ui_pMultiLE->setText(m_pRole->getMultiplicity());
 
     // Visibility
     Uml::Visibility scope = m_pRole->getVisibility();
-    if( scope == Uml::Visibility::Public )
-        m_pPublicRB -> setChecked( true );
-    else if( scope == Uml::Visibility::Private )
-        m_pPrivateRB -> setChecked( true );
-    else if( scope == Uml::Visibility::Protected )
-        m_pProtectedRB -> setChecked( true );
-    else if( scope == Uml::Visibility::Implementation )
-        m_pImplementationRB -> setChecked( true );
+    if ( scope == Uml::Visibility::Public )
+        ui_pPublicRB -> setChecked( true );
+    else if ( scope == Uml::Visibility::Private )
+        ui_pPrivateRB -> setChecked( true );
+    else if ( scope == Uml::Visibility::Protected )
+        ui_pProtectedRB -> setChecked( true );
+    else if ( scope == Uml::Visibility::Implementation )
+        ui_pImplementationRB -> setChecked( true );
 
     // Changeability
     Uml::Changeability_Type changeability = m_pRole->getChangeability();
-    if( changeability == Uml::chg_Changeable )
-        m_pChangeableRB -> setChecked( true );
-    else if( changeability == Uml::chg_Frozen )
-        m_pFrozenRB -> setChecked( true );
+    if ( changeability == Uml::chg_Changeable )
+        ui_pChangeableRB -> setChecked( true );
+    else if ( changeability == Uml::chg_Frozen )
+        ui_pFrozenRB -> setChecked( true );
     else
-        m_pAddOnlyRB -> setChecked( true );
+        ui_pAddOnlyRB -> setChecked( true );
 
     // Documentation
-    //
-    m_pDocTE-> setText(m_pRole-> getDoc());
-    //m_pDocTE->setWordWrap(QMultiLineEdit::WidgetWidth);
+    ui_pDocTE-> setText(m_pRole-> getDoc());
+    //ui_pDocTE->setWordWrap(QMultiLineEdit::WidgetWidth);
 }
 
-void UMLRoleProperties::updateObject() {
+void UMLRoleProperties::updateObject()
+{
+    if (m_pRole) {
 
-    if(m_pRole) {
-
-        // block signals to save work load..we only need to emit modified once,
+        // block signals to save work load. we only need to emit modified once,
         // not each time we update an attribute of the association. I suppose
         // we could check to see IF anything changed, but thats a lot more code,
-        // and not much gained. THis way is easier, if less 'beautiful'. -b.t.
+        // and not much gained. This way is easier, if less 'beautiful'. -b.t.
 
         m_pRole->blockSignals(true);
 
         // set props
-        m_pRole->setName(m_pRoleLE->text());
-        m_pRole->setMultiplicity(m_pMultiLE->text());
+        m_pRole->setName(ui_pRoleLE->text());
+        m_pRole->setMultiplicity(ui_pMultiLE->text());
 
-        if(m_pPrivateRB->isChecked())
+        if (ui_pPrivateRB->isChecked())
             m_pRole->setVisibility(Uml::Visibility::Private);
-        else if(m_pProtectedRB->isChecked())
+        else if (ui_pProtectedRB->isChecked())
             m_pRole->setVisibility(Uml::Visibility::Protected);
-        else if(m_pPublicRB->isChecked())
+        else if (ui_pPublicRB->isChecked())
             m_pRole->setVisibility(Uml::Visibility::Public);
-        else if(m_pImplementationRB->isChecked())
+        else if (ui_pImplementationRB->isChecked())
             m_pRole->setVisibility(Uml::Visibility::Implementation);
 
-        if(m_pFrozenRB->isChecked())
+        if (ui_pFrozenRB->isChecked())
             m_pRole->setChangeability(Uml::chg_Frozen);
-        else if(m_pAddOnlyRB->isChecked())
+        else if (ui_pAddOnlyRB->isChecked())
             m_pRole->setChangeability(Uml::chg_AddOnly);
         else
             m_pRole->setChangeability(Uml::chg_Changeable);
 
-        m_pRole->setDoc(m_pDocTE->text());
+        m_pRole->setDoc(ui_pDocTE->text());
 
         m_pRole->blockSignals(false);
 
         m_pRole->emitModified();
 
     } //end if m_pRole
-
 }
 
 
