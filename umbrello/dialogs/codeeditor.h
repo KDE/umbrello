fc35b4e367bfbe68b1dfa16361e1e0a7d7c2b5f4
Gopala Krishna A <krishna.ggk@gmail.com>
2008-07-08 21:33:51 +0000

    Merged revisions 829236 via svnmerge from svn+ssh://svn.kde.org/home/kde/trunk/KDE/kdesdk/umbrello
    ........
      r829236 | fischer | 2008-07-08 02:57:06 +0530 (Tue, 08 Jul 2008) | 1 line
      
      Krazy: replacing QTabWidget by KTabWidget.
    ........
    
    svn path=/branches/work/soc-umbrello/; revision=829700

--- umbrello/dialogs/codeeditor.h
+++ umbrello/dialogs/codeeditor.h
@@ -1,19 +1,10 @@
-
 /***************************************************************************
-                          codeeditor.h  -  description
-                             -------------------
-    begin                : Fri Aug 1 2003
-    copyright            : (C) 2003 by Brian Thomas
-    email                : brian.thomas@gsfc.nasa.gov
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
+ *   copyright (C) 2003 Brian Thomas  <brian.thomas@gsfc.nasa.gov>         *
  *   copyright (C) 2004-2008                                               *
  *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
  ***************************************************************************/
