159a43b532792c16905e85560af03e853aa2ed24
Andi Fischer <andi.fischer@hispeed.ch>
2008-08-14 12:36:15 +0000

    Fixing compile warning.
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=847015

--- umbrello/dialogs/activitydialog.cpp
+++ umbrello/dialogs/activitydialog.cpp
@@ -97,7 +97,7 @@ void ActivityDialog::applyPage( KPageWidgetItem *item )
     if ( item == pageItemGeneral )
     {
         m_pActivityWidget->setName( m_GenPageWidgets.nameLE->text() );
-        m_pActivityWidget->setDoc( m_GenPageWidgets.docTE->text() );
+        m_pActivityWidget->setDoc( m_GenPageWidgets.docTE->toPlainText() );
         m_pActivityWidget->setPreText( m_GenPageWidgets.preLE->text() );
         m_pActivityWidget->setPostText( m_GenPageWidgets.postLE->text() );
 
