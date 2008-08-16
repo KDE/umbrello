a68b0836fe04cfd8ed0e1e047ee31bb8e7ce92d4
Andi Fischer <andi.fischer@hispeed.ch>
2008-08-16 20:46:40 +0000

    Replacing all q3 widgets.
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=848076

--- umbrello/dialogs/notedialog.cpp
+++ umbrello/dialogs/notedialog.cpp
@@ -1,31 +1,29 @@
 /***************************************************************************
- *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   copyright (C) 2002-2007                                               *
+ *   copyright (C) 2002-2008                                               *
  *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
  ***************************************************************************/
 
 // own header
 #include "notedialog.h"
 
-// qt/kde includes
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <qlineedit.h>
-#include <q3groupbox.h>
-#include <q3textedit.h>
-#include <qlayout.h>
-#include <qlabel.h>
+// kde includes
 #include <klocale.h>
 #include <kmessagebox.h>
+#include <ktextedit.h>
 
+// qt includes
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QGroupBox>
 
 NoteDialog::NoteDialog( QWidget * parent, NoteWidget * pNote )
-    : KDialog(parent) {
+    : KDialog(parent)
+{
     setCaption( i18n("Note Documentation") );
     setButtons( Help | Ok | Cancel );
     setDefaultButton( Ok );
@@ -37,28 +35,31 @@ NoteDialog::NoteDialog( QWidget * parent, NoteWidget * pNote )
 
     QFrame *frame = new QFrame( this );
     setMainWidget( frame );
-    m_pDocGB = new Q3GroupBox(i18n("Documentation"), frame);
+    m_pDocGB = new QGroupBox(i18n("Documentation"), frame);
     QVBoxLayout * mainLayout = new QVBoxLayout(frame);
-    mainLayout -> addWidget(m_pDocGB);
-    mainLayout -> setSpacing(10);
-    mainLayout -> setMargin(margin);
+    mainLayout->addWidget(m_pDocGB);
+    mainLayout->setSpacing(10);
+    mainLayout->setMargin(margin);
 
     QHBoxLayout * docLayout = new QHBoxLayout(m_pDocGB);
-    docLayout -> setSpacing(10);
-    docLayout -> setMargin(margin);
+    docLayout->setSpacing(10);
+    docLayout->setMargin(margin);
 
-    m_pDocTE = new Q3TextEdit( m_pDocGB );
-    m_pDocTE -> setFocus();
-    docLayout -> addWidget( m_pDocTE );
-    m_pDocTE -> setText( pNote -> getDoc() );
+    m_pDocTE = new KTextEdit( m_pDocGB );
+    m_pDocTE->setFocus();
+    docLayout->addWidget( m_pDocTE );
+    m_pDocTE->setText( pNote->getDoc() );
     setMinimumSize(330, 160);
     connect(this,SIGNAL(okClicked()),this,SLOT(slotOk()));
 }
 
-NoteDialog::~NoteDialog() {}
+NoteDialog::~NoteDialog()
+{
+}
 
-void NoteDialog::slotOk() {
-    m_pNoteWidget -> setDoc( m_pDocTE -> text() );
+void NoteDialog::slotOk()
+{
+    m_pNoteWidget->setDoc( m_pDocTE->toPlainText() );
     accept();
 }
 
