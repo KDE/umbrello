591259b802f14f97e1860a2fa102c428105a513c
Rafael Fernández López <ereslibre@kde.org>
2008-08-27 15:43:00 +0000

    Correctly make the toolbars & docks be restored through kdelibs
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=853351

--- umbrello/uml.cpp
+++ umbrello/uml.cpp
@@ -131,7 +131,9 @@ UMLApp::UMLApp(QWidget* parent) : KXmlGuiWindow(parent)
     // call this here because the statusBar is shown/hidden by setupGUI()
     initStatusBar();
     // use the absolute path to your umbrelloui.rc file for testing purpose in setupGUI();
-    setupGUI();
+    StandardWindowOptions opt = Default;
+    opt &= ~Save;
+    setupGUI( opt );
     initView();
     initClip();
     readOptions();
@@ -160,6 +162,8 @@ UMLApp::UMLApp(QWidget* parent) : KXmlGuiWindow(parent)
     m_refactoringAssist   = NULL;
     m_commoncodegenpolicy = new CodeGenerationPolicy();
     m_imageExporterAll    = new UMLViewImageExporterAll();
+
+    setAutoSaveSettings();
 }
 
 UMLApp::~UMLApp()
@@ -660,10 +664,6 @@ void UMLApp::initView()
 
     tabifyDockWidget(m_documentationDock, m_cmdHistoryDock);
     //tabifyDockWidget(m_cmdHistoryDock, m_propertyDock);
-
-    QByteArray dockConfig;
-    dockConfig += UmbrelloSettings::dockConfig().toAscii();
-    restoreState(dockConfig); //reposition all the DockWindows to their saved positions
 }
 
 void UMLApp::openDocumentFile(const KUrl& url)
@@ -862,8 +862,6 @@ void UMLApp::readProperties(const KConfigGroup & cfg)     //:TODO: applyMainWind
 
 bool UMLApp::queryClose()
 {
-    QByteArray dockConfig = saveState();
-    UmbrelloSettings::setDockConfig( dockConfig );
     return m_doc->saveModified();
 }
 
@@ -1054,8 +1052,6 @@ void UMLApp::slotFileQuit()
 {
     slotStatusMsg(i18n("Exiting..."));
     if (m_doc->saveModified()) {
-        QByteArray dockConfig = saveState();
-        UmbrelloSettings::setDockConfig( dockConfig );
         saveOptions();
         qApp->quit();
     }
