2ebe98eb0aa8ef67e07e555f22fad2ff775ce380
Andi Fischer <andi.fischer@hispeed.ch>
2011-07-08 15:55:44 +0000

    Some comments fixed.
    svn path=/branches/work/soc-umbrello/; revision=1240452

--- umbrello/umlscene.cpp
+++ umbrello/umlscene.cpp
@@ -2690,7 +2690,7 @@ void UMLScene::createAutoAttributeAssociation(UMLClassifier *type, UMLAttribute 
         UMLClassifier *dt = static_cast<UMLClassifier*>(type);
         // if the Datatype is a reference (pointer) type
         if (dt->isReference()) {
-            //UMLObject::Association_Type assocType = Uml::AssociationType::Composition;
+            //Uml::AssociationType assocType = Uml::AssociationType::Composition;
             UMLClassifier *c = dt->originType();
             UMLWidget *w = c ? findWidget(c->id()) : 0;
             // if the referenced type has a widget representation on this view
