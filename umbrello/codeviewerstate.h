592d85a472e4b6deca066795c104d89d73bed26a
Andi Fischer <andi.fischer@hispeed.ch>
2011-09-24 10:10:11 +0000

    Krazy issues 3 (copyright) fixed, unneeded includes removed and some formatting changed.
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=1255284

--- umbrello/codeviewerstate.h
+++ umbrello/codeviewerstate.h
@@ -1,19 +1,18 @@
 /***************************************************************************
- *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   copyright (C) 2004-2007                                               *
+ *   copyright (C) 2004-2011                                               *
  *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
  ***************************************************************************/
 
 #ifndef CODEVIEWERSTATE_H
 #define CODEVIEWERSTATE_H
 
-#include <qcolor.h>
-#include <qfont.h>
+#include <QtGui/QColor>
+#include <QtGui/QFont>
 
 namespace Settings {
 
