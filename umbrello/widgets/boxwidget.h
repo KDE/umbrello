eda177242230a243e64093dc2ad201fa991c3745
Ralf Habacker <ralf.habacker@gmail.com>
2011-12-13 12:04:05 +0000

    reduced differences between trunk and soc-umbrello branch
    A related patch will be applied to trunk.
    
    svn path=/branches/work/soc-umbrello/; revision=1268449

--- umbrello/widgets/boxwidget.h
+++ umbrello/widgets/boxwidget.h
@@ -1,11 +1,10 @@
 /***************************************************************************
- *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   copyright (C) 2003-2006                                               *
+ *   copyright (C) 2003-2011                                               *
  *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
  ***************************************************************************/
 
@@ -31,10 +30,27 @@
 class BoxWidget : public UMLWidget
 {
 public:
+    /**
+     * Constructs a BoxWidget.
+     *
+     * @param id                The ID to assign (-1 will prompt a new ID.)
+     */
     explicit BoxWidget(Uml::IDType id = Uml::id_None);
+
+    /**
+     * destructor
+     */
     virtual ~BoxWidget();
 
+    /**
+     * Draws a rectangle.
+     */
     virtual void paint(QPainter *p, const QStyleOptionGraphicsItem *item, QWidget *w);
+
+    /**
+     * Saves the widget to the "boxwidget" XMI element.
+     * Note: For loading from XMI, the inherited parent method is used.
+     */
     virtual void saveToXMI(QDomDocument& qDoc, QDomElement& qElement);
 };
 
