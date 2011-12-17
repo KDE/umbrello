cec82d103101f9bc6f161f8d57e621ba92be4ffd
Ralf Habacker <ralf.habacker@gmail.com>
2011-12-17 11:56:35 +0000

    implemented iteration over the list view items using keyboard and show documentation of related item
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=1268945

--- umbrello/umllistview.cpp
+++ umbrello/umllistview.cpp
@@ -329,19 +329,15 @@ void UMLListView::keyPressEvent(QKeyEvent *ke)
                 deleteItem(item);
             }
         }
-#if 0
-        // using keyboard to iterate through the items and showing doc do not work as expected
         else if (k == Qt::Key_Up || k == Qt::Key_Down) {
             UMLListViewItem *current = static_cast<UMLListViewItem*>(currentItem());
-            UMLListViewItem *item = static_cast<UMLListViewItem*>((k == Qt::Key_Down) ? current->itemBelow() : current->itemAbove());
-            setSelected(current, false);
+            UMLListViewItem *item = static_cast<UMLListViewItem*>((k == Qt::Key_Down) ? itemBelow(current) : itemAbove(current));
             if (item) {
-                setSelected(item, true);
+                setCurrentItem(item);
                 m_doc->changeCurrentView(item->getID());
                 UMLApp::app()->docWindow()->showDocumentation(item->umlObject(), false);
             }
         }
-#endif
         else {
             QTreeWidget::keyPressEvent(ke); // let parent handle it
         }
