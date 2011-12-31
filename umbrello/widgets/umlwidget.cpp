912da2a84774d4847fac5e1253ea1cd7e50fbed7
Ralf Habacker <ralf.habacker@gmail.com>
2011-12-31 13:15:50 +0000

    fixed bug having too small widget width on a common place
    merged commit 1268739 and 1268761 from trunk
    
    svn path=/branches/KDE/4.7/kdesdk/umbrello/; revision=1270975

--- umbrello/widgets/umlwidget.cpp
+++ umbrello/widgets/umlwidget.cpp
@@ -898,9 +898,8 @@ void UMLWidget::updateComponentSize()
 {
     if (m_pDoc->loading())
         return;
-    const int w = getWidth();
-    const int h = getHeight();
-    setSize(w, h);
+    QSize size = calculateSize();
+    setSize(size.width(), size.height());
     adjustAssocs(getX(), getY());    // adjust assoc lines
 }
 
