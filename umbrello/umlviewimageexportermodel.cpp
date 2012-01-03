3e75de2f1b9775fe98a03464d42337b63dadbe90
Andi Fischer <andi.fischer@hispeed.ch>
2012-01-03 11:23:10 +0000

    Resize command of the widgets during a SVG export disabled. Thanks to Kip Warner for giving notice.
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=1271154

--- umbrello/umlviewimageexportermodel.cpp
+++ umbrello/umlviewimageexportermodel.cpp
@@ -493,7 +493,10 @@ bool UMLViewImageExporterModel::exportViewToSvg(UMLScene* scene, const QString &
     // make sure the widget sizes will be according to the
     // actually used printer font, important for getDiagramRect()
     // and the actual painting
-    scene->forceUpdateWidgetFontMetrics(&painter);
+//    scene->forceUpdateWidgetFontMetrics(&painter);
+    //Note: The above was commented out because other exportViewTo...
+    //      do not have it and it forces a resize of the widgets,
+    //      which is not correctly implemented for now.
 
     painter.translate(-rect.x(),-rect.y());
     scene->getDiagram(rect, painter);
@@ -503,7 +506,8 @@ bool UMLViewImageExporterModel::exportViewToSvg(UMLScene* scene, const QString &
     exportSuccessful = true;
 
     // next painting will most probably be to a different device (i.e. the screen)
-    scene->forceUpdateWidgetFontMetrics(0);
+//    scene->forceUpdateWidgetFontMetrics(0);
+    //Note: See comment above.
 
     DEBUG(DBG_IEM) << "saving to file " << fileName << " successful=" << exportSuccessful;
     return exportSuccessful;
