1834c3fccd51559f3b44e49494a5f0481b3ecbc9
Andi Fischer <andi.fischer@hispeed.ch>
2012-01-05 08:51:35 +0000

    Cleanup of code.
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=1271761

--- umbrello/widgets/notewidget.h
+++ umbrello/widgets/notewidget.h
@@ -4,7 +4,7 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   copyright (C) 2002-2010                                               *
+ *   copyright (C) 2002-2012                                               *
  *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
  ***************************************************************************/
 
@@ -36,6 +36,7 @@ class NoteWidget : public UMLWidget
 public:
     friend class NoteWidgetController;
 
+    /// This enum type is used to specify the type of note.
     enum NoteType
     {
         Normal,
@@ -43,51 +44,42 @@ public:
         PostCondition,
         Transformation
     };
-    explicit NoteWidget(UMLScene * scene, NoteWidget::NoteType noteType = Normal, Uml::IDType id = Uml::id_None );
+
+    explicit NoteWidget(UMLScene * scene, NoteWidget::NoteType noteType = Normal,
+                        Uml::IDType id = Uml::id_None);
     virtual ~NoteWidget();
 
-    void paint(QPainter & p, int offsetX, int offsetY);
+    static NoteType stringToNoteType(const QString& noteType);
 
-    NoteType getNoteType() const;
-    NoteType getNoteType(const QString& noteType) const;
-    void setNoteType( NoteType noteType );
-    void setNoteType( const QString& noteType );
+    NoteType noteType() const;
+    void setNoteType(NoteType noteType);
+    void setNoteType(const QString& noteType);
 
     QString documentation() const;
     void setDocumentation(const QString &newText);
 
-    Uml::IDType getDiagramLink() const;
+    Uml::IDType diagramLink() const;
     void setDiagramLink(Uml::IDType viewID);
 
-    // this method is obsolate 
-    void setFont(QFont font);
-
-    // this method is obsolate 
-    void setX(int x);
-    // this method is obsolate 
-    void setY(int y);
+    virtual void paint(QPainter & p, int offsetX, int offsetY);
 
     void askForNoteType(UMLWidget* &targetWidget);
 
-    bool loadFromXMI( QDomElement & qElement );
-    void saveToXMI( QDomDocument & qDoc, QDomElement & qElement );
-
+    virtual bool loadFromXMI(QDomElement & qElement);
+    virtual void saveToXMI(QDomDocument & qDoc, QDomElement & qElement);
 
 public Q_SLOTS:
     void slotMenuSelection(QAction* action);
 
 protected:
-    QSize calculateSize();
-    void drawText(QPainter * p = NULL, int offsetX = 0, int offsetY = 0);
-    void drawTextWordWrap(QPainter * p = NULL, int offsetX = 0, int offsetY = 0);
-
-    Uml::IDType m_diagramLink;  ///< Data loaded/saved.
-    NoteType    m_noteType;     ///< Type of note.
-    QString     m_type;         ///< Label to see the note's type.
-
+    virtual QSize calculateSize();
+    void drawText(QPainter * p = 0, int offsetX = 0, int offsetY = 0);
+    void drawTextWordWrap(QPainter * p = 0, int offsetX = 0, int offsetY = 0);
 
 private:
-    QString m_Text;
+    Uml::IDType m_diagramLink;  ///< The diagram/scene this note links to.
+    NoteType    m_noteType;     ///< The type of note. @see NoteWidget::NoteType
+    QString     m_Text;
 };
 
 #endif
