3405a84645dca2f34d53b66995ecd1ec0d8b4988
Oliver Kellogg <okellogg@users.sourceforge.net>
2012-03-04 11:52:45 +0000

    umbrello/umlcanvasobject.cpp - removeAssociationEnd(): Call UMLDoc:removeAssociation() on the assoc.
    umbrello/umldoc.cpp
    - removeUMLObject(): Remove associations that this object may participate in.
    
    BUG: 293042
    
    svn path=/trunk/KDE/kdesdk/umbrello/; revision=1283458

--- umbrello/umlcanvasobject.cpp
+++ umbrello/umlcanvasobject.cpp
@@ -13,6 +13,7 @@
 
 // local includes
 #include "debug_utils.h"
+#include "model_utils.h"
 #include "uml.h"
 #include "umldoc.h"
 #include "classifier.h"
@@ -101,10 +102,9 @@ bool UMLCanvasObject::addAssociationEnd(UMLAssociation* assoc)
  */
 bool UMLCanvasObject::hasAssociation(UMLAssociation* assoc)
 {
-    if (m_List.count(assoc) > 0) {
-        return true;
-    }
-    return false;
+    uint cnt = m_List.count(assoc);
+    uDebug() << "count is " << cnt;
+    return (cnt > 0);
 }
 
 /**
@@ -119,6 +119,7 @@ int UMLCanvasObject::removeAssociationEnd(UMLAssociation * assoc)
         uDebug() << "can not find given assoc " << assoc << " in list";
         return -1;
     }
+    UMLApp::app()->document()->removeAssociation(assoc, false);
     UMLObject::emitModified();
     emit sigAssociationEndRemoved(assoc);
     return m_List.count();
--- umbrello/umldoc.cpp
+++ umbrello/umldoc.cpp
@@ -1523,6 +1523,10 @@ void UMLDoc::setCurrentRoot(Uml::ModelType rootType)
  */
 void UMLDoc::removeUMLObject(UMLObject* umlobject)
 {
+    if (umlobject == NULL) {
+        uError() << "called with NULL parameter";
+        return;
+    }
     UMLApp::app()->docWindow()->updateDocumentation(true);
     UMLObject::ObjectType type = umlobject->baseType();
 
@@ -1567,6 +1571,33 @@ void UMLDoc::removeUMLObject(UMLObject* umlobject)
         } else {
             UMLPackage* pkg = umlobject->umlPackage();
             if (pkg) {
+                // Remove associations that this object may participate in.
+                UMLCanvasObject *c = dynamic_cast<UMLCanvasObject*>(umlobject);
+                if (c) {
+                    // In the current implementation, all associations live in the
+                    // root folder.
+                    UMLPackage* rootPkg = Model_Utils::rootPackage(c);
+                    if (rootPkg == NULL) {
+                        uError() << umlobject->name() << ": root package is not set !";
+                        return;
+                    }
+                    UMLObjectList rootObjects = rootPkg->containedObjects();
+                    // Store the associations to remove in a buffer because we
+                    // should not remove elements from m_objectList while it is
+                    // being iterated over.
+                    UMLAssociationList assocsToRemove;
+                    foreach (UMLObject *obj , rootObjects) {
+                        if (obj->baseType() == UMLObject::ot_Association) {
+                            UMLAssociation *assoc = static_cast<UMLAssociation*>(obj);
+                            if (c->hasAssociation(assoc)) {
+                                assocsToRemove.append(assoc);
+                            }
+                        }
+                    }
+                    foreach (UMLAssociation *a, assocsToRemove) {
+                        removeAssociation(a, false);
+                    }
+                }
                 pkg->removeObject(umlobject);
             } else {
                 uError() << umlobject->name() << ": parent package is not set !";
