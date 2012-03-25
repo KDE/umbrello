7bf31bf8c0fd62674bd06dfa5397b022cedd6ec2
Andi Fischer <andi.fischer@hispeed.ch>
2012-03-25 20:39:50 +0000

    Unnecessary include removed.
    svn path=/branches/work/soc-umbrello/; revision=1287129

--- umbrello/widgets/widget_factory.h
+++ umbrello/widgets/widget_factory.h
@@ -4,20 +4,19 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   copyright (C) 2006                                                    *
+ *   copyright (C) 2006-2012                                               *
  *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
  ***************************************************************************/
 
 #ifndef WIDGET_FACTORY_H
 #define WIDGET_FACTORY_H
 
-#include <qstring.h>
+#include <QString>
 
 // forward declarations
-class UMLView;
 class UMLObject;
-class UMLWidget;
 class UMLScene;
+class UMLWidget;
 
 /**
  * Widget factory methods.
