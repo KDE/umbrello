65bc130341d0410574a83ac7501979bd4e030fc9
Ralf Habacker <ralf.habacker@gmail.com>
2012-04-03 18:59:43 +0000

    Reduced differences to trunk.
    svn path=/branches/work/soc-umbrello/; revision=1288098

--- umbrello/version.h
+++ umbrello/version.h
@@ -4,21 +4,28 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   copyright (C) 2002-2008                                               *
+ *   copyright (C) 2002-2012                                               *
  *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
  ***************************************************************************/
 
 #ifndef UMBRELLO_VERSION_H
 #define UMBRELLO_VERSION_H
 
+#include "revision.h"
+
 #include <kdeversion.h>
 
 inline QByteArray umbrelloVersion()
 {
-    return QString("%1.%2.%3")
+    QString versionStr = QString("%1.%2.%3")
         .arg(KDE::versionMajor()-2)
         .arg(KDE::versionMinor())
-        .arg(KDE::versionRelease()).toLatin1();
+        .arg(KDE::versionRelease());
+    QString revisionStr = QString("-r%1").arg(REVISION);
+    if (revisionStr != "-r0") {
+        versionStr.append(revisionStr);
+    }
+    return versionStr.toLatin1();
 }
 
 #endif
