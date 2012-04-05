f9ed4c3a73a0bff98e0ad9933c10731965d73264
Ralf Habacker <ralf.habacker@gmail.com>
2012-04-05 16:34:01 +0000

    Reduced differences to trunk.
    svn path=/branches/work/soc-umbrello/; revision=1288336

--- umbrello/widgets/linkwidget.cpp
+++ umbrello/widgets/linkwidget.cpp
@@ -4,7 +4,7 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   copyright (C) 2004-2011                                               *
+ *   copyright (C) 2004-2012                                               *
  *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
  ***************************************************************************/
 
@@ -12,11 +12,10 @@
 #include "linkwidget.h"
 
 // app includes
-#include "umlview.h"
-#include "umlobject.h"
 #include "classifier.h"
 #include "operation.h"
 #include "uml.h"
+#include "umlobject.h"
 #include "umlscene.h"
 
 LinkWidget::LinkWidget()
