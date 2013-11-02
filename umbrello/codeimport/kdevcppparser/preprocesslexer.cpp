f26d52a079bbcb3943f4e104f1e97abe43742481
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2013-11-03 01:21:49 +0200

    kdevcppparser: Always define the `identity' unary function.
    _Identity is an internal, libstdc++-specific struct that is not to be relied
    upon by external code.
    
    preprocesslexer.cpp used to assume GCC (with libstdc++) and MSVC were the
    only compilers available, and defined its own _Identity implementation for
    the latter. It breaks things if any other compiler or STL implementation
    (such as clang with libc++) is used.
    
    The easiest fix is to just unconditionally define the `identity' unary
    function and use it regardless of whether _Identity is defined or not.
    
    Reviewed-by: Andi Fischer <andi.fischer@hispeed.ch>
    (cherry picked from commit 5f0f05caac4e65b77a8e5754e4b7e999185581f5)
    
    Conflicts:
    	umbrello/codeimport/kdevcppparser/preprocesslexer.cpp

--- umbrello/codeimport/kdevcppparser/preprocesslexer.cpp
+++ umbrello/codeimport/kdevcppparser/preprocesslexer.cpp
@@ -41,13 +41,11 @@
 
 #define PREPROCESSLEXER_DEBUG
 
-#ifdef Q_CC_MSVC
 template <class _Tp>
-struct _Identity : public std::unary_function<_Tp, _Tp> {
+struct identity : public std::unary_function<_Tp,_Tp> {
   _Tp& operator()(_Tp& __x) const { return __x; }
   const _Tp& operator()(const _Tp& __x) const { return __x; }
 };
-#endif
 
 template <class _Tp>
 struct tilde : public std::unary_function<_Tp, _Tp> {
@@ -858,7 +856,7 @@ int PreprocessLexer::macroPrimary()
         if (!l_hit)
             result = 0;
     } else {
-        boost::function < int (int) > l_op = _Identity<int>();
+        boost::function < int (int) > l_op = identity<int>();
         if (m_source.parse(ch_p('+')
                            | ch_p('-')[var(l_op) = std::negate<int>()]
                            | ch_p('!')[var(l_op) = std::logical_not<int>()]
