40377231fd1fbb2fa23947a3c382996c9178dd16
Ralf Habacker <ralf.habacker@freenet.de>
2013-11-20 16:47:10 +0100

    Fix 'select all' menu or key stroke command.
    BUG:327870
    FIXED-IN:4.11.4

--- umbrello/umlscene.cpp
+++ umbrello/umlscene.cpp
@@ -1466,7 +1466,7 @@ void UMLScene::deleteSelection()
  */
 void UMLScene::selectAll()
 {
-    selectWidgets(0, 0, width(), height());
+    selectWidgets(sceneRect().x(), sceneRect().y(), width(), height());
 }
 
 /**
