3f6ccedbce47201c0ab95ea4772fd1962dd3d091
Ralf Habacker <ralf.habacker@sag.eu>
2013-11-20 16:11:16 +0100

    Cross plattform compile fix

--- umbrello/codeimport/kdevcppparser/preprocesslexer.cpp
+++ umbrello/codeimport/kdevcppparser/preprocesslexer.cpp
@@ -837,7 +837,7 @@ int PreprocessLexer::macroPrimary()
         if (!l_hit)
             result = 0;
     } else {
-        boost::function < int (int) > l_op = identity<int>();
+        boost::function < int (int) > l_op = ::identity<int>();
         if (m_source.parse(ch_p('+')
                            | ch_p('-')[var(l_op) = std::negate<int>()]
                            | ch_p('!')[var(l_op) = std::logical_not<int>()]
