0f7746fcf3484095fb90f2ed07c22761fe4cf6bb
Ralf Habacker <ralf.habacker@freenet.de>
2013-11-20 18:27:12 +0100

    Fix 'Select all' right border too.
    BUG:327870

--- umbrello/umlscene.cpp
+++ umbrello/umlscene.cpp
@@ -1496,7 +1496,7 @@ void UMLScene::resizeSelection()
  */
 void UMLScene::selectAll()
 {
-    selectWidgets(sceneRect().x(), sceneRect().y(), width(), height());
+    selectWidgets(sceneRect().left(), sceneRect().top(), sceneRect().right(), sceneRect().bottom());
 }
 
 /**
