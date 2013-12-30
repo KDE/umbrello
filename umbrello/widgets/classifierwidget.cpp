KDE/umbrello

commit 5b0bf1ecf393c4c1d9cfb7fe7f058e4a54ee1177

Undo stack and visual properties improvements
The multi-widget context menu allows setting visual properties ("Show") for a
selection. Unlike the previous "toggle" behaviour it is now possible to (for
example) hide all operations with one menu action.

Also fixed:
* applying the properties dialog without changes added a few undo commands to the stack
* the "Delete" shortcut is renamed to "Delete Diagram" because that is what it does
* mixed capitalization cmd/Cmd, is now always "Cmd"
* changes to the following widget options are now tracked in the undo stack:
** visual properties
** use fill color
** line width
** font

BUG: 329175
BUG: 329176

umbrello/widgets/classifierwidget.cpp
@@ -14,6 +14,7 @@
 // app includes
 #include "associationwidget.h"
 #include "classifier.h"
+#include "cmds.h"
 #include "debug_utils.h"
 #include "listpopupmenu.h"
 #include "object_factory.h"
@@ -42,10 +43,10 @@ ClassifierWidget::ClassifierWidget(UMLScene * scene, UMLClassifier *c)
     m_pAssocWidget(0)
 {
     const Settings::OptionState& ops = m_scene->optionState();
-    setVisualProperty(ShowVisibility, ops.classState.showVisibility);
-    setVisualProperty(ShowOperations, ops.classState.showOps);
-    setVisualProperty(ShowPublicOnly, ops.classState.showPublicOnly);
-    setVisualProperty(ShowPackage,    ops.classState.showPackage);
+    setVisualPropertyCmd(ShowVisibility, ops.classState.showVisibility);
+    setVisualPropertyCmd(ShowOperations, ops.classState.showOps);
+    setVisualPropertyCmd(ShowPublicOnly, ops.classState.showPublicOnly);
+    setVisualPropertyCmd(ShowPackage,    ops.classState.showPackage);
     m_attributeSignature = Uml::SignatureType::ShowSig;
     /*:TODO:
     setVisualProperty(ShowOperationSignature, ops.classState.showOpSig);
@@ -62,9 +63,9 @@ ClassifierWidget::ClassifierWidget(UMLScene * scene, UMLClassifier *c)
     else
         m_operationSignature = Uml::SignatureType::SigNoVis;
 
-    setVisualProperty(ShowAttributes, ops.classState.showAtts);
-    setVisualProperty(ShowStereotype, ops.classState.showStereoType);
-    setVisualProperty(DrawAsCircle, false);
+    setVisualPropertyCmd(ShowAttributes, ops.classState.showAtts);
+    setVisualPropertyCmd(ShowStereotype, ops.classState.showStereoType);
+    setVisualPropertyCmd(DrawAsCircle, false);
 
     setShowAttSigs(ops.classState.showAttSig);
 
@@ -142,6 +143,8 @@ bool ClassifierWidget::visualProperty(VisualProperty property) const
 /**
  * A convenient method to set and reset individual VisualProperty
  *
+ * Undo command.
+ *
  * @param property The property to be set/reset.
  * @param enable   True/false to set/reset. (default = true)
  *
@@ -149,6 +152,22 @@ bool ClassifierWidget::visualProperty(VisualProperty property) const
  *       ShowOperationSignature specially.
  */
 void ClassifierWidget::setVisualProperty(VisualProperty property, bool enable)
+{
+    if (visualProperty(property) != enable) {
+        UMLApp::app()->executeCommand(new Uml::CmdChangeVisualProperty(this, property, enable));
+    }
+}
+
+/**
+ * A convenient method to set and reset individual VisualProperty
+ *
+ * @param property The property to be set/reset.
+ * @param enable   True/false to set/reset. (default = true)
+ *
+ * @note This method handles ShowAttributeSignature and
+ *       ShowOperationSignature specially.
+ */
+void ClassifierWidget::setVisualPropertyCmd(VisualProperty property, bool enable)
 {
     // Handle ShowAttributeSignature and ShowOperationSignature
     // specially.
@@ -960,57 +979,46 @@ void ClassifierWidget::slotMenuSelection(QAction* action)
             break;
         }
     case ListPopupMenu::mt_Show_Operations:
-    case ListPopupMenu::mt_Show_Operations_Selection:
         toggleVisualProperty(ShowOperations);
         break;
 
     case ListPopupMenu::mt_Show_Attributes:
-    case ListPopupMenu::mt_Show_Attributes_Selection:
         toggleVisualProperty(ShowAttributes);
         break;
 
     case ListPopupMenu::mt_Show_Public_Only:
-    case ListPopupMenu::mt_Show_Public_Only_Selection:
         toggleVisualProperty(ShowPublicOnly);
         break;
 
     case ListPopupMenu::mt_Show_Operation_Signature:
-    case ListPopupMenu::mt_Show_Operation_Signature_Selection:
         toggleVisualProperty(ShowOperationSignature);
         break;
 
     case ListPopupMenu::mt_Show_Attribute_Signature:
-    case ListPopupMenu::mt_Show_Attribute_Signature_Selection:
         toggleVisualProperty(ShowAttributeSignature);
         break;
 
     case ListPopupMenu::mt_Visibility:
-    case ListPopupMenu::mt_Visibility_Selection:
         toggleVisualProperty(ShowVisibility);
         break;
 
     case ListPopupMenu::mt_Show_Packages:
-    case ListPopupMenu::mt_Show_Packages_Selection:
         toggleVisualProperty(ShowPackage);
         break;
 
     case ListPopupMenu::mt_Show_Stereotypes:
-    case ListPopupMenu::mt_Show_Stereotypes_Selection:
         toggleVisualProperty(ShowStereotype);
         break;
 
     case ListPopupMenu::mt_DrawAsCircle:
-    case ListPopupMenu::mt_DrawAsCircle_Selection:
         toggleVisualProperty(DrawAsCircle);
         break;
 
     case ListPopupMenu::mt_ChangeToClass:
-    case ListPopupMenu::mt_ChangeToClass_Selection:
         changeToClass();
         break;
 
     case ListPopupMenu::mt_ChangeToInterface:
-    case ListPopupMenu::mt_ChangeToInterface_Selection:
         changeToInterface();
         break;
 

umbrello/CMakeLists.txt
@@ -300,11 +300,14 @@ set(libcmds_SRCS
     cmds/generic/cmd_createUMLObject.cpp
     cmds/generic/cmd_renameUMLObject.cpp
 
-    cmds/widget/cmd_changeFontSelection.cpp
+    cmds/widget/cmd_changeFont.cpp
     cmds/widget/cmd_changeTextColor.cpp
     cmds/widget/cmd_changeLineColor.cpp
+    cmds/widget/cmd_changeLineWidth.cpp
     cmds/widget/cmd_changeFillColor.cpp
+    cmds/widget/cmd_changeUseFillColor.cpp
     cmds/widget/cmd_changeMultiplicity.cpp
+    cmds/widget/cmd_changeVisualProperty.cpp
     cmds/widget/cmd_createWidget.cpp
     cmds/widget/cmd_setName.cpp
     cmds/widget/cmd_setTxt.cpp

umbrello/cmds.h
@@ -33,10 +33,13 @@
  * Widgets
  ************************************************************/
 #include "cmds/widget/cmd_changeFillColor.h"
-#include "cmds/widget/cmd_changeFontSelection.h"
+#include "cmds/widget/cmd_changeFont.h"
 #include "cmds/widget/cmd_changeLineColor.h"
+#include "cmds/widget/cmd_changeLineWidth.h"
+#include "cmds/widget/cmd_changeUseFillColor.h"
 #include "cmds/widget/cmd_changeMultiplicity.h"
 #include "cmds/widget/cmd_changeTextColor.h"
+#include "cmds/widget/cmd_changeVisualProperty.h"
 #include "cmds/widget/cmd_createWidget.h"
 #include "cmds/widget/cmd_setName.h"
 #include "cmds/widget/cmd_setTxt.h"

umbrello/cmds/widget/cmd_changeFillColor.cpp
@@ -37,7 +37,7 @@ namespace Uml
     {
         UMLScene* scene = m_widget->umlScene();
         if (scene && scene->widgetOnDiagram(m_widget->id())) {
-            m_widget->setFillColorcmd(m_color);
+            m_widget->setFillColorCmd(m_color);
         }
     }
 
@@ -45,7 +45,7 @@ namespace Uml
     {
         UMLScene* scene = m_widget->umlScene();
         if (scene && scene->widgetOnDiagram(m_widget->id())) {
-            m_widget->setFillColorcmd(m_oldColor);
+            m_widget->setFillColorCmd(m_oldColor);
         }
     }
 }

umbrello/cmds/widget/cmd_changeFont.cpp
@@ -8,11 +8,9 @@
  *   Umbrello UML Modeller Authors <umbrello-devel@kde.org>                *
  ***************************************************************************/
 
-#include "cmd_changeFontSelection.h"
+#include "cmd_changeFont.h"
 
 // app includes
-#include "umldoc.h"
-#include "umlscene.h"
 #include "umlwidget.h"
 
 // kde includes
@@ -20,26 +18,23 @@
 
 namespace Uml
 {
-    CmdChangeFontSelection::CmdChangeFontSelection(UMLDoc* doc, UMLScene* scene, QFont fon)
+    CmdChangeFont::CmdChangeFont(UMLWidget* widget, QFont font)
     {
-        UMLWidget * widget = scene->getFirstMultiSelectedWidget();
         setText(i18n("Change font : %1", widget->name()));
-        m_doc = doc;
-        m_scene = scene;
-        m_newFont = fon;
+
+        m_widget = widget;
+        m_newFont = font;
         m_oldFont = widget->font();
     }
 
-    void CmdChangeFontSelection::undo()
+    void CmdChangeFont::undo()
     {
-        m_scene->selectionSetFont(m_oldFont);
-        m_doc->setModified(true);
+        m_widget->setFontCmd(m_oldFont);
     }
 
-    void CmdChangeFontSelection::redo()
+    void CmdChangeFont::redo()
     {
-        m_scene->selectionSetFont(m_newFont);
-        m_doc->setModified(true);
+        m_widget->setFontCmd(m_newFont);
     }
 
 }

umbrello/cmds/widget/cmd_changeFont.h
@@ -8,30 +8,29 @@
  *   Umbrello UML Modeller Authors <umbrello-devel@kde.org>                *
  ***************************************************************************/
 
-#ifndef CMD_CHANGELINECOLOR_H
-#define CMD_CHANGELINECOLOR_H
+#ifndef CMD_CHANGEFONT_H
+#define CMD_CHANGEFONT_H
 
-#include <QColor>
+#include "umlwidget.h"
+
+#include <QFont>
 #include <QUndoCommand>
-
-class UMLWidget;
 
 namespace Uml
 {
-    class CmdChangeLineColor : public QUndoCommand
+    class CmdChangeFont: public QUndoCommand
     {
-    public:
-        CmdChangeLineColor(UMLWidget *w, const QColor& col);
-        ~CmdChangeLineColor();
-        void redo();
-        void undo();
+        public:
+            CmdChangeFont(UMLWidget* widget, QFont fon);
 
-    private:
-        UMLWidget* m_widget;
-        QColor m_oldColor;
-        QColor m_newColor;
-        bool m_oldUsesDiagramValue;
+            void undo();
+            void redo();
+
+        private:
+            UMLWidget* m_widget;
+            QFont     m_newFont;
+            QFont     m_oldFont;
     };
 }
 
-#endif // CMD_CHANGELINECOLOR_H
+#endif // CMD_CHANGEFONT_H

umbrello/cmds/widget/cmd_changeLineWidth.cpp
@@ -8,7 +8,7 @@
  *   Umbrello UML Modeller Authors <umbrello-devel@kde.org>                *
  ***************************************************************************/
 
-#include "cmd_changeLineColor.h"
+#include "cmd_changeLineWidth.h"
 
 // app includes
 #include "umlscene.h"
@@ -20,35 +20,32 @@
 namespace Uml
 {
 
-    CmdChangeLineColor::CmdChangeLineColor(UMLWidget *w, const QColor& col)
+    CmdChangeLineWidth::CmdChangeLineWidth(UMLWidget *w, const uint width)
       : m_widget(w),
-        m_newColor(col)
+        m_newWidth(width)
     {
         Q_ASSERT(w != 0);
-        setText(i18n("Change line color : %1", w->name()));
-        m_oldColor= w->lineColor() ;
-        m_oldUsesDiagramValue = w->usesDiagramLineColor();
+        setText(i18n("Change line width : %1", w->name()));
+        m_oldWidth = w->lineWidth() ;
     }
 
-    CmdChangeLineColor::~CmdChangeLineColor()
+    CmdChangeLineWidth::~CmdChangeLineWidth()
     {
     }
 
-    void CmdChangeLineColor::redo()
-    {
-        UMLScene* scene = m_widget->umlScene();
-        if (scene && scene->widgetOnDiagram(m_widget->id())) {
-            m_widget->setLineColorCmd(m_newColor);
-        }
-    }
-
-    void CmdChangeLineColor::undo()
+    void CmdChangeLineWidth::redo()
     {
         UMLScene* scene = m_widget->umlScene();
         if (scene && scene->widgetOnDiagram(m_widget->id())) {
-            m_widget->setLineColorCmd(m_oldColor);
-            m_widget->setUsesDiagramLineColor(m_oldUsesDiagramValue);
+            m_widget->setLineWidthCmd(m_newWidth);
         }
     }
 
+    void CmdChangeLineWidth::undo()
+    {
+        UMLScene* scene = m_widget->umlScene();
+        if (scene && scene->widgetOnDiagram(m_widget->id())) {
+            m_widget->setLineWidthCmd(m_oldWidth);
+        }
+    }
 }

umbrello/cmds/widget/cmd_changeLineWidth.h
@@ -8,30 +8,28 @@
  *   Umbrello UML Modeller Authors <umbrello-devel@kde.org>                *
  ***************************************************************************/
 
-#ifndef CMD_CHANGELINECOLOR_H
-#define CMD_CHANGELINECOLOR_H
+#ifndef CMD_CHANGELINEWIDTH_H
+#define CMD_CHANGELINEWIDTH_H
 
-#include <QColor>
 #include <QUndoCommand>
 
 class UMLWidget;
 
 namespace Uml
 {
-    class CmdChangeLineColor : public QUndoCommand
+    class CmdChangeLineWidth : public QUndoCommand
     {
     public:
-        CmdChangeLineColor(UMLWidget *w, const QColor& col);
-        ~CmdChangeLineColor();
+        CmdChangeLineWidth(UMLWidget *w, const uint width);
+        ~CmdChangeLineWidth();
         void redo();
         void undo();
 
     private:
         UMLWidget* m_widget;
-        QColor m_oldColor;
-        QColor m_newColor;
-        bool m_oldUsesDiagramValue;
+        uint m_oldWidth;
+        uint m_newWidth;
     };
 }
 
-#endif // CMD_CHANGELINECOLOR_H
+#endif // CMD_CHANGELINEWIDTH_H

commit 682d56ab7315337c381dcc499aa7cb9b9a13eab7

Fix loading of visual properties from XMI
This regressed in 5b0bf1e, e.g. UmbrelloArchitecture/Overview.xmi did not look
alright (undo commands skip the operation if widgets are not on a diagram, at
least until we merge upcoming undo/redo fixes).

umbrello/widgets/classifierwidget.cpp
@@ -917,12 +917,12 @@ bool ClassifierWidget::loadFromXMI(QDomElement & qElement)
     QString showscope = qElement.attribute("showscope", "0");
     QString drawascircle = qElement.attribute("drawascircle", "0");
 
-    setVisualProperty(ShowAttributes, (bool)showatts.toInt());
-    setVisualProperty(ShowOperations, (bool)showops.toInt());
-    setVisualProperty(ShowPublicOnly, (bool)showpubliconly.toInt());
-    setVisualProperty(ShowPackage,    (bool)showpackage.toInt());
-    setVisualProperty(ShowVisibility, (bool)showscope.toInt());
-    setVisualProperty(DrawAsCircle,   (bool)drawascircle.toInt());
+    setVisualPropertyCmd(ShowAttributes, (bool)showatts.toInt());
+    setVisualPropertyCmd(ShowOperations, (bool)showops.toInt());
+    setVisualPropertyCmd(ShowPublicOnly, (bool)showpubliconly.toInt());
+    setVisualPropertyCmd(ShowPackage,    (bool)showpackage.toInt());
+    setVisualPropertyCmd(ShowVisibility, (bool)showscope.toInt());
+    setVisualPropertyCmd(DrawAsCircle,   (bool)drawascircle.toInt());
 
     m_attributeSignature = Uml::SignatureType::fromInt(showattsigs.toInt());
     m_operationSignature = Uml::SignatureType::fromInt(showopsigs.toInt());