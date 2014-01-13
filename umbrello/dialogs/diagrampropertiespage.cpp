e7784cc3cf85319bc70bea9512c9f4809041d7f0
Ralf Habacker <ralf.habacker@freenet.de>
2014-01-13 11:29:25 +0100

    Remove duplicated 'Operation Signature' checkbox, which is already defined in the style page.
    CCBUG: 132360

--- umbrello/dialogs/diagrampropertiespage.cpp
+++ umbrello/dialogs/diagrampropertiespage.cpp
@@ -33,7 +33,6 @@ DiagramPropertiesPage::DiagramPropertiesPage(QWidget *parent, UMLScene *scene)
 
     ui_diagramName->setText(scene->name());
     ui_zoom->setValue(scene->activeView()->currentZoom());
-    ui_showOpSigs->setChecked(scene->showOpSig());
 
     ui_checkBoxShowGrid->setChecked(scene->isSnapGridVisible());
     ui_snapToGrid->setChecked(scene->snapToGrid());
@@ -107,7 +106,6 @@ void DiagramPropertiesPage::apply()
     m_scene->setSnapToGrid(ui_snapToGrid->isChecked());
     m_scene->setSnapComponentSizeToGrid(ui_snapComponentSizeToGrid->isChecked());
     m_scene->setSnapGridVisible(ui_checkBoxShowGrid->isChecked());
-    m_scene->setShowOpSig(ui_showOpSigs->isChecked());
     if (m_scene->type() == Uml::DiagramType::Sequence) {
         m_scene->setAutoIncrementSequence(ui_autoIncrementSequence->isChecked());
     }
