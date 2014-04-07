KDE/umbrello

commit f182ee32f72ce2754b68b76b412afe86ca137e17

Event handler contextMenuEvent used instead of old setupPopupMenu functions.

umbrello/widgets/signalwidget.cpp
@@ -53,7 +53,7 @@ SignalWidget::SignalWidget(UMLScene *scene, SignalType signalType, Uml::ID::Type
 }
 
 /**
- * destructor
+ * Destructor.
  */
 SignalWidget::~SignalWidget()
 {
@@ -311,15 +311,19 @@ void SignalWidget::saveToXMI( QDomDocument & qDoc, QDomElement & qElement )
  */
 void SignalWidget::slotMenuSelection(QAction* action)
 {
-    bool ok = false;
-    QString name = m_Text;
-
-    ListPopupMenu::MenuType sel = m_pMenu->getMenuType(action);
+    ListPopupMenu::MenuType sel = ListPopupMenu::typeFromAction(action);
     switch( sel ) {
     case ListPopupMenu::mt_Rename:
-        name = KInputDialog::getText( i18n("Enter signal name"), i18n("Enter the signal name :"), m_Text, &ok );
-        if( ok && name.length() > 0 )
-            setName(name);
+        {
+            bool ok = false;
+            QString name = m_Text;
+            name = KInputDialog::getText(i18n("Enter signal name"),
+                                         i18n("Enter the signal name :"),
+                                         m_Text, &ok );
+            if (ok && name.length() > 0) {
+                setName(name);
+            }
+        }
         break;
 
     default:

umbrello/widgets/widgetbase.h
@@ -15,11 +15,14 @@
 
 #include <QColor>
 #include <QDomDocument>
+#include <QFont>
 #include <QGraphicsObject>
 #include <QObject>
 #include <QPainter>
 
 // forward declarations
+class ListPopupMenu;
+class QAction;
 class UMLDoc;
 class UMLObject;
 class UMLScene;
@@ -88,6 +91,9 @@ public:
     QString documentation() const;
     void setDocumentation(const QString& doc);
 
+    QString name() const;
+    void setName(const QString &strName);
+
     QColor lineColor() const;
     virtual void setLineColor(const QColor& color);
 
@@ -118,6 +124,11 @@ public:
     bool usesDiagramUseFillColor() const;
     void setUsesDiagramUseFillColor(bool state);
 
+    QFont font() const;
+    void setFont(const QFont& font);
+
+    virtual void showPropertiesDialog();
+
     virtual bool loadFromXMI(QDomElement &qElement);
     virtual void saveToXMI(QDomDocument &qDoc, QDomElement &qElement);
 
@@ -132,11 +143,18 @@ public:
     virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = 0);
     QT_DEPRECATED virtual void draw(QPainter & p, int offsetX, int offsetY);
 
+public Q_SLOTS:
+    virtual void slotMenuSelection(QAction *trigger);
+
 protected:
+    virtual void contextMenuEvent(QGraphicsSceneContextMenuEvent *event);
+    virtual void setupContextMenuActions(ListPopupMenu &menu);
+
     WidgetType  m_baseType;  ///< Type of widget.
     UMLScene   *m_scene;
     UMLObject  *m_umlObject;
     QString     m_Doc;   ///< Only used if m_umlObject is not set.
+    QString     m_Text;
     QRectF      m_rect;  ///< widget size
 
     /**
@@ -149,13 +167,10 @@ protected:
     QColor m_textColor;  ///< Color of the text of the widget. Is saved to XMI.
     QColor m_lineColor;  ///< Color of the lines of the widget. Is saved to XMI.
     QColor m_fillColor;  ///< color of the background of the widget
-
-    uint m_lineWidth;  ///< Width of the lines of the widget. Is saved to XMI.
-
-    /**
-     * This flag indicates if the UMLWidget uses the Diagram FillColour
-     */
-    bool m_useFillColor;
+    QBrush m_brush;
+    QFont  m_font;
+    uint   m_lineWidth;  ///< Width of the lines of the widget. Is saved to XMI.
+    bool   m_useFillColor;  ///< flag indicates if the UMLWidget uses the Diagram FillColour
 
     /**
      * true by default, false if the colors have

umbrello/CMakeLists.txt
@@ -371,7 +371,7 @@ set(libwidgets_SRCS
 set (libcontroller_SRCS
     controller/floatingtextwidgetcontroller.cpp
     controller/messagewidgetcontroller.cpp
-    controller/notewidgetcontroller.cpp
+#    controller/notewidgetcontroller.cpp
     controller/objectwidgetcontroller.cpp
     controller/umlwidgetcontroller.cpp
 )

umbrello/controller/messagewidgetcontroller.cpp
@@ -151,14 +151,14 @@ void MessageWidgetController::constrainMovementForAllWidgets(UMLSceneValue &diff
  *
  * @param me The QGraphicsSceneMouseEvent which triggered the double click event.
  */
-void MessageWidgetController::doMouseDoubleClick(QGraphicsSceneMouseEvent* me)
-{
-    Q_UNUSED(me);
-    if (m_messageWidget->m_pFText != NULL) {
-        QAction* action = m_messageWidget->m_pMenu->getAction(ListPopupMenu::mt_Select_Operation);
-        m_messageWidget->m_pFText->slotMenuSelection(action);
-    }
-}
+//void MessageWidgetController::doMouseDoubleClick(QGraphicsSceneMouseEvent* me)
+//{
+//    Q_UNUSED(me);
+//    if (m_messageWidget->m_pFText != NULL) {
+//        QAction* action = m_messageWidget->m_pMenu->getAction(ListPopupMenu::mt_Select_Operation);
+//        m_messageWidget->m_pFText->slotMenuSelection(action);
+//    }
+//}
 
 /**
  * Constrains the vertical position of the message widget so it doesn't go

umbrello/controller/messagewidgetcontroller.h
@@ -62,7 +62,7 @@ protected:
 
     virtual void constrainMovementForAllWidgets(UMLSceneValue &diffX, UMLSceneValue &diffY);
 
-    virtual void doMouseDoubleClick(QGraphicsSceneMouseEvent *me);
+//    virtual void doMouseDoubleClick(QGraphicsSceneMouseEvent *me);
 
 private:
 

umbrello/controller/notewidgetcontroller.cpp
@@ -1,58 +1,0 @@
-/***************************************************************************
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   copyright (C) 2006-2013                                               *
- *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
- ***************************************************************************/
-
-// app includes
-#include "notewidgetcontroller.h"
-#include "notewidget.h"
-#include "uml.h"
-#include "umldoc.h"
-#include "listpopupmenu.h"
-
-/**
- * Constructor for NoteWidgetController.
- *
- * @param noteWidget The NoteWidget which uses the controller.
- */
-NoteWidgetController::NoteWidgetController(NoteWidget *noteWidget)
-  : UMLWidgetController(noteWidget)
-{
-    m_noteWidget = noteWidget;
-}
-
-/**
- * Destructor for NoteWidgetController.
- */
-NoteWidgetController::~NoteWidgetController()
-{
-}
-
-/**
- * Overridden from UMLWidgetController.
- * Executes the action for double click in the widget.
- * Shows the dialog to change the text of the note.
- *
- * @param me The QGraphicsSceneMouseEvent which triggered the double click event.
- */
-void NoteWidgetController::doMouseDoubleClick(QGraphicsSceneMouseEvent* me)
-{
-    Q_UNUSED(me);
-    //TODO Copied from old code. What it does?
-    if (m_noteWidget->m_diagramLink == Uml::ID::None) {
-        // setup popup menu if not already created.
-        if ( !m_noteWidget->m_pMenu )
-            m_noteWidget->setupPopupMenu();
-
-        QAction* action = m_noteWidget->m_pMenu->getAction(ListPopupMenu::mt_Rename);
-        m_noteWidget->slotMenuSelection(action);
-    } else {
-        UMLDoc *umldoc = UMLApp::app()->document();
-        umldoc->changeCurrentView(m_noteWidget->m_diagramLink);
-    }
-}

umbrello/controller/notewidgetcontroller.h
@@ -1,45 +1,0 @@
-/***************************************************************************
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   copyright (C) 2006-2013                                               *
- *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
- ***************************************************************************/
-
-#ifndef NOTEWIDGETCONTROLLER_H
-#define NOTEWIDGETCONTROLLER_H
-
-#include "umlwidgetcontroller.h"
-
-class NoteWidget;
-
-/**
- * Controller for NoteWidget.
- *
- * MouseMove and MouseRelease execute the base code and then specific code
- * for note widget.
- *
- * Double click behaviour is edit the text of the note.
- *
- * @author Umbrello UML Modeller Authors <uml-devel@lists.sourceforge.net>
- */
-class NoteWidgetController : public UMLWidgetController
-{
-public:
-
-    explicit NoteWidgetController(NoteWidget* noteWidget);
-    virtual ~NoteWidgetController();
-
-protected:
-
-    virtual void doMouseDoubleClick(QGraphicsSceneMouseEvent *me);
-
-private:
-
-    NoteWidget* m_noteWidget;  ///< The note widget which uses the controller.
-
-};
-
-#endif

umbrello/controller/umlwidgetcontroller.cpp
@@ -4,7 +4,7 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   copyright (C) 2006-2012                                               *
+ *   copyright (C) 2006-2013                                               *
  *   Umbrello UML Modeller Authors <uml-devel@uml.sf.net>                  *
  ***************************************************************************/
 
@@ -343,7 +343,7 @@ void UMLWidgetController::mouseReleaseEvent(QGraphicsSceneMouseEvent *me)
     } else if (me->button() == Qt::RightButton) {
         if (m_rightButtonDown) {
             m_rightButtonDown = false;
-            showPopupMenu(me);
+            //:TODO: was showPopupMenu(me);
         } else if (m_leftButtonDown) {
 #ifdef Q3CANVAS_IMPLEMENTATION
             //Cancel move/edit
@@ -526,10 +526,10 @@ void UMLWidgetController::constrainMovementForAllWidgets(UMLSceneValue &diffX, U
  */
 void UMLWidgetController::doMouseDoubleClick(QGraphicsSceneMouseEvent *)
 {
-    if (!m_widget || !m_widget->m_pMenu)
-        return;
-    QAction* action = m_widget->m_pMenu->getAction(ListPopupMenu::mt_Properties);
-    m_widget->slotMenuSelection(action);
+//    if (!m_widget || !m_widget->m_pMenu)
+//        return;
+//    QAction* action = m_widget->m_pMenu->getAction(ListPopupMenu::mt_Properties);
+//    m_widget->slotMenuSelection(action);
 }
 
 /**
@@ -842,21 +842,6 @@ UMLScenePoint UMLWidgetController::getPositionDifference(QGraphicsSceneMouseEven
     const int diffX = newPoint.x() - m_widget->x();
     const int diffY = newPoint.y() - m_widget->y();
     return UMLScenePoint(diffX, diffY);
-}
-
-/**
- * Shows the widget popup menu where the mouse event points to.
- *
- * @param me The QGraphicsSceneMouseEvent which triggered the showing.
- */
-void UMLWidgetController::showPopupMenu(QGraphicsSceneMouseEvent *me)
-{
-    //TODO why this condition?
-    if (m_widget->m_pMenu) {
-        return;
-    }
-    ListPopupMenu* menu = m_widget->setupPopupMenu();
-    menu->popup(me->screenPos());
 }
 
 /**

commit 9f77894e719a0658af8b3fb039c98f5591656fbc

Fix not called SignalWidget::setName() because of removal of virtual keyword on WidgetBase::setName().
This break has been introduced with commit f182ee32f72ce2754b68b76b412afe86ca137e17.

umbrello/widgets/signalwidget.cpp
@@ -93,7 +93,7 @@ void SignalWidget::paint(QPainter *painter, const QStyleOptionGraphicsItem *opti
             painter->setFont(font);
             painter->drawText(SIGNAL_MARGIN, textStartY,
                            w - SIGNAL_MARGIN * 2, fontHeight,
-                           Qt::AlignCenter, getName());
+                           Qt::AlignCenter, name());
             setPenFromSettings(painter);
         }
         break;
@@ -118,7 +118,7 @@ void SignalWidget::paint(QPainter *painter, const QStyleOptionGraphicsItem *opti
             painter->setFont(font);
             painter->drawText(SIGNAL_MARGIN, textStartY,
                            w - SIGNAL_MARGIN * 2 + (w/3), fontHeight,
-                           Qt::AlignCenter, getName());
+                           Qt::AlignCenter, name());
             setPenFromSettings(painter);
         }
         break;
@@ -184,7 +184,7 @@ void SignalWidget::setY(qreal newY)
  */
 void SignalWidget::setName(const QString &strName)
 {
-    m_Text = strName;
+    UMLWidget::setName(strName);
     updateGeometry();
     if (signalType() == SignalWidget::Time) {
         if (!m_pName) {
@@ -197,14 +197,6 @@ void SignalWidget::setName(const QString &strName)
         else
             m_pName->setText(m_Text);
     }
-}
-
-/**
- * Returns the name of the Signal.
- */
-QString SignalWidget::getName() const
-{
-    return m_Text;
 }
 
 /**

umbrello/widgets/signalwidget.h
@@ -46,7 +46,6 @@ public:
     void setY(qreal newY);
 
     virtual void setName(const QString &strName);
-    virtual QString getName() const;
 
     SignalType signalType() const;
     QString signalTypeStr() const;

umbrello/widgets/widgetbase.h
@@ -95,7 +95,7 @@ public:
     void setDocumentation(const QString& doc);
 
     QString name() const;
-    void setName(const QString &strName);
+    virtual void setName(const QString &strName);
 
     QColor lineColor() const;
     virtual void setLineColor(const QColor& color);