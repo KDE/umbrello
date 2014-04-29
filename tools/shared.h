5c8eceddd8026399753d19db4dcd45d3e3ff17d0
Ralf Habacker <ralf.habacker@freenet.de>
2014-04-29 13:42:48 +0200

    Add command line tools po2xmi and xmi2pot.
    - xmi2pot extracts all messages from a xmi file into a pot file format
    - po2xmi takes an untranslated xmi file and a translated po file to generate a localized xmi file
    
    BUG:340385

---
+++ tools/shared.h
@@ -0,0 +1,44 @@
+/***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   copyright (C) 2014                                                    *
+ *   Umbrello UML Modeller Authors <umbrello-devel@kde.org>                *
+ *                                                                         *
+ *   @author Ralf Habacker <ralf.habacker@freenet.de>                      *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef SHARED_H
+#define SHARED_H
+
+#include <QString>
+#include <QStringList>
+#include <QMap>
+
+QStringList XMILanguagesAttributes();
+
+class POEntry {
+public:
+    QString tagName;
+    QString attributeName;
+    QString value;
+    QList<int> lineNumbers;
+};
+
+typedef QMap<QString, POEntry> POMap;
+
+
+bool extractAttributesFromXMI(const char *fileName, const QStringList &attributes, POMap &result);
+
+QString toGetTextString(const QString &message);
+
+typedef QMap<QString,QString> TranslationMap;
+
+bool fetchPoFile(const QString &fileName, TranslationMap &map);
+
+QString applyTranslationToXMIFile(const char *fileName, const QStringList &attributes, TranslationMap &translations);
+
+#endif
