KDE/umbrello

commit 359986044b0af22616089bd93590a128fa21ba8a

Port++
svn path=/trunk/KDE/kdesdk/umbrello/; revision=553891

umbrello/dialogs/overwritedialogue.cpp
@@ -23,17 +23,25 @@
 
 #include "overwritedialogue.h"
 
-OverwriteDialogue::OverwriteDialogue(QString fileName, QString outputDirectory, bool applyToAllRemaining, QWidget* parent, const char* name):
-KDialogBase(Plain, i18n("Destination File Already Exists"), Ok|Apply|Cancel, Yes, parent, name) {
+OverwriteDialogue::OverwriteDialogue(QString fileName, QString outputDirectory, bool applyToAllRemaining, QWidget* parent, const char* name)
+    :KDialog(parent) {
+    setCaption( i18n("Destination File Already Exists") );
+    setButtons( Help | Ok | Cancel );
+    setDefaultButton( Yes );
+    setModal( true );
+    enableButtonSeparator( true );
 
-    QVBoxLayout* layout = new QVBoxLayout( plainPage() );
+    QFrame *frame = new QFrame( this );
+    setMainWidget( frame );
+
+    QVBoxLayout* layout = new QVBoxLayout( frame );
     layout->setSpacing( spacingHint() );
     layout->setMargin( 0 );
 
-    QLabel* dialogueLabel = new QLabel(i18n("The file %1 already exists in %2.\n\nUmbrello can overwrite the file, generate a similar\nfile name or not generate this file.", fileName, outputDirectory), plainPage() );
+    QLabel* dialogueLabel = new QLabel(i18n("The file %1 already exists in %2.\n\nUmbrello can overwrite the file, generate a similar\nfile name or not generate this file.", fileName, outputDirectory), frame );
     layout->addWidget(dialogueLabel);
 
-    m_applyToAllRemaining = new QCheckBox( i18n("&Apply to all remaining files"), plainPage() );
+    m_applyToAllRemaining = new QCheckBox( i18n("&Apply to all remaining files"), frame );
     m_applyToAllRemaining->setChecked(applyToAllRemaining);
     layout->addWidget(m_applyToAllRemaining);
 

umbrello/codegenerator.cpp
@@ -30,7 +30,7 @@
 #include <kdebug.h>
 #include <klocale.h>
 #include <kmessagebox.h>
-#include <kdialogbase.h>
+#include <kdialog.h>
 #include <kapplication.h>
 
 // app includes

umbrello/codegenerators/simplecodegenerator.cpp
@@ -26,7 +26,7 @@
 #include <klocale.h>
 #include <kdebug.h>
 #include <kmessagebox.h>
-#include <kdialogbase.h>
+#include <kdialog.h>
 #include <kapplication.h>
 // app includes
 #include "../dialogs/overwritedialogue.h"
@@ -162,14 +162,14 @@ QString SimpleCodeGenerator::overwritableName(UMLClassifier* concept, QString na
         break;
     case CodeGenerationPolicy::Ask:               //ask if we can overwrite
         switch(overwriteDialogue.exec()) {
-        case KDialogBase::Yes:  //overwrite file
+        case KDialog::Yes:  //overwrite file
             if ( overwriteDialogue.applyToAllRemaining() ) {
                 commonPolicy->setOverwritePolicy(CodeGenerationPolicy::Ok);
             } else {
                 m_applyToAllRemaining = false;
             }
             break;
-        case KDialogBase::No: //generate similar name
+        case KDialog::No: //generate similar name
             suffix = 1;
             while (1) {
                 filename = name + "__" + QString::number(suffix) + ext;
@@ -183,7 +183,7 @@ QString SimpleCodeGenerator::overwritableName(UMLClassifier* concept, QString na
                 m_applyToAllRemaining = false;
             }
             break;
-        case KDialogBase::Cancel: //don't output anything
+        case KDialog::Cancel: //don't output anything
             if ( overwriteDialogue.applyToAllRemaining() ) {
                 commonPolicy->setOverwritePolicy(CodeGenerationPolicy::Cancel);
             } else {

umbrello/dialogs/notedialog.cpp
@@ -29,12 +29,21 @@
 #include <q3textedit.h>
 
 
-NoteDialog::NoteDialog( QWidget * parent, NoteWidget * pNote ) : KDialogBase(Plain, i18n("Note Documentation"), Help | Ok | Cancel , Ok, parent, "_NOTEDIALOG_", true, true) {
+NoteDialog::NoteDialog( QWidget * parent, NoteWidget * pNote )
+    : KDialog(parent) {
+    setCaption( i18n("Note Documentation") );
+    setButtons( Help | Ok | Cancel );
+    setDefaultButton( Ok );
+    setModal( true );
+    enableButtonSeparator( true );
+
     m_pNoteWidget = pNote;
     int margin = fontMetrics().height();
 
-    m_pDocGB = new Q3GroupBox(i18n("Documentation"), plainPage());
-    QVBoxLayout * mainLayout = new QVBoxLayout(plainPage());
+    QFrame *frame = new QFrame( this );
+    setMainWidget( frame );
+    m_pDocGB = new Q3GroupBox(i18n("Documentation"), frame);
+    QVBoxLayout * mainLayout = new QVBoxLayout(frame);
     mainLayout -> addWidget(m_pDocGB);
     mainLayout -> setSpacing(10);
     mainLayout -> setMargin(margin);

umbrello/dialogs/notedialog.h
@@ -15,7 +15,7 @@
 #ifndef NOTEDIALOG_H
 #define NOTEDIALOG_H
 //kde includes
-#include <kdialogbase.h>
+#include <kdialog.h>
 //app includes
 #include "../notewidget.h"
 
@@ -27,7 +27,7 @@ class Q3GroupBox;
 class QLineEdit;
 class Q3TextEdit;
 
-class NoteDialog : public KDialogBase {
+class NoteDialog : public KDialog {
 public:
     /**
      *  Constructs an NoteDialog.

umbrello/dialogs/overwritedialogue.h
@@ -15,7 +15,7 @@
 #ifndef OVERWRITEDIALOGUE_H
 #define OVERWRITEDIALOGUE_H
 
-#include <kdialogbase.h>
+#include <kdialog.h>
 
 class QCheckBox;
 
@@ -30,7 +30,7 @@ class QCheckBox;
  * @author Jonathan Riddell <jr@jriddell.org>
  * Bugs and comments to uml-devel@lists.sf.net or http://bugs.kde.org
  */
-class OverwriteDialogue: public KDialogBase {
+class OverwriteDialogue: public KDialog {
     Q_OBJECT
 
 public:

umbrello/dialogs/parmpropdlg.cpp
@@ -37,8 +37,14 @@
 #include "../object_factory.h"
 
 ParmPropDlg::ParmPropDlg(QWidget * parent, UMLDoc * doc, UMLAttribute * a)
-        : KDialogBase(Plain, i18n("Parameter Properties"), Help | Ok | Cancel , Ok, parent, "_PARMPROPDLG_", true, true)
+        : KDialog(parent)
 {
+    setCaption( i18n("Parameter Properties") );
+    setButtons( Help | Ok | Cancel );
+    setDefaultButton( Ok );
+    setModal( true );
+    enableButtonSeparator( true );
+
     m_pUmldoc = doc;
     m_pAtt = a;
     QString type, text, name, initialValue;
@@ -53,11 +59,13 @@ ParmPropDlg::ParmPropDlg(QWidget * parent, UMLDoc * doc, UMLAttribute * a)
     int margin = fontMetrics().height();
     setMinimumSize(300, 400);
     //disableResize();
-    QVBoxLayout * topLayout = new QVBoxLayout(plainPage());
+    QFrame *frame = new QFrame( this );
+    setMainWidget( frame );
+    QVBoxLayout * topLayout = new QVBoxLayout(frame);
     topLayout -> setSpacing(10);
     topLayout -> setMargin(margin);
 
-    m_pParmGB = new Q3GroupBox(i18n("Properties"), plainPage());
+    m_pParmGB = new Q3GroupBox(i18n("Properties"), frame);
     topLayout -> addWidget(m_pParmGB);
 
     QGridLayout * propLayout = new QGridLayout(m_pParmGB);
@@ -83,7 +91,7 @@ ParmPropDlg::ParmPropDlg(QWidget * parent, UMLDoc * doc, UMLAttribute * a)
                                     m_pStereoTypeL, i18n("&Stereotype name:"),
                                     m_pStereoTypeLE, m_pAtt->getStereotype() );
 
-    m_pKind =  new Q3ButtonGroup(i18n("Passing Direction"), plainPage());
+    m_pKind =  new Q3ButtonGroup(i18n("Passing Direction"), frame);
     m_pKind->setExclusive(true);
     QToolTip::add(m_pKind, i18n("\"in\" is a readonly parameter, \"out\" is a writeonly parameter and \"inout\" is a parameter for reading and writing."));
 
@@ -101,7 +109,7 @@ ParmPropDlg::ParmPropDlg(QWidget * parent, UMLDoc * doc, UMLAttribute * a)
 
     topLayout -> addWidget(m_pKind);
 
-    m_pDocGB = new Q3GroupBox(i18n("Documentation"), plainPage());
+    m_pDocGB = new Q3GroupBox(i18n("Documentation"), frame);
     QHBoxLayout * docLayout = new QHBoxLayout(m_pDocGB);
     docLayout -> setMargin(margin);
 

umbrello/dialogs/parmpropdlg.h
@@ -15,7 +15,7 @@
 #ifndef PARMPROPDLG_H
 #define PARMPROPDLG_H
 
-#include <kdialogbase.h>
+#include <kdialog.h>
 #include <kcombobox.h>
 #include <q3groupbox.h>
 #include <qlabel.h>
@@ -38,7 +38,7 @@ class UMLDoc;
  * @author Paul Hensgen	<phensgen@techie.com>
  * Bugs and comments to uml-devel@lists.sf.net or http://bugs.kde.org
  */
-class ParmPropDlg : public KDialogBase {
+class ParmPropDlg : public KDialog {
 public:
     /**
      * Constructs a ParmPropDlg.

commit 2de99c94f83d80b2723a9fb47ade7485687a29e6

Regression fix: Show all defined buttons in OverwriteDialogue.
The regression has been introduced with commit 359986044b0af22616089bd93590a128fa21ba8a.

CCBUG:334084

umbrello/dialogs/overwritedialogue.cpp
@@ -30,7 +30,7 @@ OverwriteDialogue::OverwriteDialogue(
   : KDialog(parent)
 {
     setCaption(i18n("Destination File Already Exists"));
-    setButtons(Help | Ok | Cancel);
+    setButtons(Ok | Apply | Cancel);
     setDefaultButton(Yes);
     setModal(true);
     showButtonSeparator(true);