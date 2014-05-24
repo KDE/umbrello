2265a6f33745c941ad7453a4677560ed7a9fd1e9
Oliver Kellogg <okellogg@users.sourceforge.net>
2014-05-24 21:45:58 +0200

    Address known issue of commit d22ed2f, > 1) The PortWidget is saved but is not restored on reloading the XMI file.
    umbrello/widgets/widget_factory.cpp
    - In function makeWidgetFromXMI() add handling of tag == "portwidget".
    
    CCBUG:115269

--- umbrello/widgets/widget_factory.cpp
+++ umbrello/widgets/widget_factory.cpp
@@ -40,6 +40,8 @@
 #include "messagewidget.h"
 #include "node.h"
 #include "nodewidget.h"
+#include "port.h"
+#include "portwidget.h"
 #include "notewidget.h"
 #include "object_factory.h"
 #include "objectnodewidget.h"
@@ -251,6 +253,9 @@ UMLWidget* makeWidgetFromXMI(const QString& tag,
         } else if (tag == "componentwidget") {
             if (validateObjType(UMLObject::ot_Component, o, id))
                 widget = new ComponentWidget(scene, static_cast<UMLComponent*>(o));
+        } else if (tag == "portwidget") {
+            if (validateObjType(UMLObject::ot_Port, o, id))
+                widget = new PortWidget(scene, static_cast<UMLPort*>(o));
         } else if (tag == "nodewidget") {
             if (validateObjType(UMLObject::ot_Node, o, id))
                 widget = new NodeWidget(scene, static_cast<UMLNode*>(o));
