aecf6fe8f04a5260ab3fae5863f9a94ed8573802
Ralf Habacker <ralf.habacker@freenet.de>
2014-06-08 18:02:22 +0200

    Fix bug "undefined reference to 'vtable for UMLAppPrivate'" reported by continous integration system.
    automoc4 seems not to be able to generate correct code for classes using signal/slots inside cpp files.

--- umbrello/uml.cpp
+++ umbrello/uml.cpp
@@ -12,6 +12,7 @@
 #include "uml.h"
 
 // app includes
+#include "uml_p.h"
 #include "umldoc.h"
 #include "umllistview.h"
 #include "umlviewlist.h"
@@ -77,17 +78,11 @@
 #include <kdeprintdialog.h>
 #include <kundostack.h>
 #include <kstatusbar.h>
-#include <ktexteditor/configinterface.h>
-#include <ktexteditor/document.h>
-#include <ktexteditor/editor.h>
-#include <ktexteditor/editorchooser.h>
-#include <ktexteditor/view.h>
 
 // qt includes
 #include <QClipboard>
 #include <QDesktopWidget>
 #include <QDockWidget>
-#include <QFileInfo>
 #include <QKeyEvent>
 #include <QLabel>
 #include <QMenu>
@@ -106,63 +101,6 @@
 #include <QListWidget>
 
 #include <cmath>
-
-/**
- * Class UMLAppPrivate holds private class members/methods
- * to reduce the size of the public class and to speed up
- * recompiling.
- * The migration to this class is not complete yet.
- **/
-class UMLAppPrivate : public QObject
-{
-    Q_OBJECT
-public:
-    UMLApp *parent;
-    FindDialog findDialog;
-    FindResults findResults;
-    QListWidget *logWindow;         ///< Logging window.
-
-    KTextEditor::Editor *editor;
-    KTextEditor::View *view;
-    KTextEditor::Document *document;
-
-    UMLAppPrivate(UMLApp *_parent)
-      : parent(_parent),
-        findDialog(_parent),
-        view(0),
-        document(0)
-    {
-        editor = KTextEditor::EditorChooser::editor();
-        logWindow = new QListWidget;
-        connect(logWindow, SIGNAL(itemDoubleClicked(QListWidgetItem *)), this, SLOT(slotLogWindowItemDoubleClicked(QListWidgetItem *)));
-    }
-
-public slots:
-    void slotLogWindowItemDoubleClicked(QListWidgetItem *item)
-    {
-        QStringList columns = item->text().split(':');
-
-        QFileInfo file(columns[0]);
-        if (!file.exists())
-            return;
-
-        document = editor->createDocument(0);
-        view = document->createView(parent);
-        view->document()->openUrl(columns[0]);
-        view->document()->setReadWrite(false);
-        view->setCursorPosition(KTextEditor::Cursor(columns[1].toInt()-1,columns[2].toInt()));
-        KTextEditor::ConfigInterface *iface = qobject_cast<KTextEditor::ConfigInterface*>(view);
-        if(iface)
-            iface->setConfigValue("line-numbers", true);
-
-        KDialog *dialog = new KDialog(parent);
-        dialog->setMainWidget(view);
-        dialog->setMinimumSize(800, 800);
-        dialog->exec();
-        delete dialog;
-        delete document;
-    }
-};
 
 /** Static pointer, holding the last created instance. */
 UMLApp* UMLApp::s_instance;
---
+++ umbrello/uml_p.h
@@ -0,0 +1,88 @@
+/***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   copyright (C) 2002-2014                                               *
+ *   Umbrello UML Modeller Authors <umbrello-devel@kde.org>                *
+ ***************************************************************************/
+
+#ifndef UMLAPP_P_H
+#define UMLAPP_P_H
+
+// app includes
+#include "finddialog.h"
+#include "findresults.h"
+
+// kde includes
+#include <ktexteditor/configinterface.h>
+#include <ktexteditor/document.h>
+#include <ktexteditor/editor.h>
+#include <ktexteditor/editorchooser.h>
+#include <ktexteditor/view.h>
+
+// qt includes
+#include <QFile>
+#include <QFileInfo>
+#include <QListWidget>
+#include <QObject>
+
+/**
+ * Class UMLAppPrivate holds private class members/methods
+ * to reduce the size of the public class and to speed up
+ * recompiling.
+ * The migration to this class is not complete yet.
+ **/
+class UMLAppPrivate : public QObject
+{
+    Q_OBJECT
+public:
+    UMLApp *parent;
+    FindDialog findDialog;
+    FindResults findResults;
+    QListWidget *logWindow;         ///< Logging window.
+
+    KTextEditor::Editor *editor;
+    KTextEditor::View *view;
+    KTextEditor::Document *document;
+
+    UMLAppPrivate(UMLApp *_parent)
+      : parent(_parent),
+        findDialog(_parent),
+        view(0),
+        document(0)
+    {
+        editor = KTextEditor::EditorChooser::editor();
+        logWindow = new QListWidget;
+        connect(logWindow, SIGNAL(itemDoubleClicked(QListWidgetItem *)), this, SLOT(slotLogWindowItemDoubleClicked(QListWidgetItem *)));
+    }
+
+public slots:
+    void slotLogWindowItemDoubleClicked(QListWidgetItem *item)
+    {
+        QStringList columns = item->text().split(':');
+
+        QFileInfo file(columns[0]);
+        if (!file.exists())
+            return;
+
+        document = editor->createDocument(0);
+        view = document->createView(parent);
+        view->document()->openUrl(columns[0]);
+        view->document()->setReadWrite(false);
+        view->setCursorPosition(KTextEditor::Cursor(columns[1].toInt()-1,columns[2].toInt()));
+        KTextEditor::ConfigInterface *iface = qobject_cast<KTextEditor::ConfigInterface*>(view);
+        if(iface)
+            iface->setConfigValue("line-numbers", true);
+
+        KDialog *dialog = new KDialog(parent);
+        dialog->setMainWidget(view);
+        dialog->setMinimumSize(800, 800);
+        dialog->exec();
+        delete dialog;
+        delete document;
+    }
+};
+
+#endif
