KDE/umbrello

commit b3fa5a7d30978df688c9448a81614f28d2a07015

KUrl --> QUrl

umbrello/docgenerators/docbookgenerator.cpp
@@ -59,10 +59,10 @@ DocbookGenerator::~DocbookGenerator()
  */
 bool DocbookGenerator::generateDocbookForProject()
 {
-  KUrl url = m_umlDoc->url();
+  QUrl url = m_umlDoc->url();
   QString fileName = url.fileName();
   fileName.remove(QRegExp(QLatin1String(".xmi$")));
-  url.setFileName(fileName);
+  url.setPath(url.path() + QLatin1Char('/') + fileName);
   uDebug() << "Exporting to directory: " << url;
   generateDocbookForProjectInto(url);
   return true;
@@ -104,7 +104,7 @@ void DocbookGenerator::slotDocbookGenerationFinished(const QString& tmpFileName)
     fileName.replace(QRegExp(QLatin1String(".xmi$")), QLatin1String(".docbook"));
     url.setPath(m_destDir.path() + QLatin1Char('/') + fileName);
 
-    KIO::Job* job = KIO::file_copy(KUrl::fromPath(tmpFileName), url, -1, KIO::Overwrite | KIO::HideProgressInfo);
+    KIO::Job* job = KIO::file_copy(QUrl(tmpFileName), url, -1, KIO::Overwrite | KIO::HideProgressInfo);
     if (KIO::NetAccess::synchronousRun(job, (QWidget*)UMLApp::app())) {
         m_umlDoc->writeToStatusBar(i18n("Docbook Generation Complete..."));
         m_pStatus = true;

umbrello/codegenerators/codegenobjectwithtextblocks.cpp
@@ -336,48 +336,7 @@ void CodeGenObjectWithTextBlocks::resetTextBlocks()
     TextBlock *tb;
     for (TextBlockListIt it(m_textblockVector); (tb = it.current()) != NULL; ++it)
         delete tb;
-     **************  else the following crash happens on loading an XMI file:
-     Invalid read of size 4
-        at 0x8134BAA: QString::QString(QString const&) (qstring.h:650)
-        by 0x8331EE5: TextBlock::getTag() const (textblock.cpp:103)
-        by 0x82C4634: ClassifierCodeDocument::findCodeClassFieldTextBlockByTag(QString const&) (classifiercodedocument.cpp:730)
-        by 0x82F3940: HierarchicalCodeBlock::findCodeClassFieldTextBlockByTag(QString const&) (hierarchicalcodeblock.cpp:366)
-        by 0x82D71DC: CodeGenObjectWithTextBlocks::loadChildTextBlocksFromNode(QDomElement&) (codegenobjectwithtextblocks.cpp:422)
-        by 0x82D6DFF: CodeGenObjectWithTextBlocks::setAttributesFromNode(QDomElement&) (codegenobjectwithtextblocks.cpp:373)
-        by 0x82F3412: HierarchicalCodeBlock::setAttributesFromNode(QDomElement&) (hierarchicalcodeblock.cpp:290)
-        by 0x82F3248: HierarchicalCodeBlock::loadFromXMI(QDomElement&) (hierarchicalcodeblock.cpp:269)
-        by 0x82D760F: CodeGenObjectWithTextBlocks::loadChildTextBlocksFromNode(QDomElement&) (codegenobjectwithtextblocks.cpp:453)
-        by 0x82D6DFF: CodeGenObjectWithTextBlocks::setAttributesFromNode(QDomElement&) (codegenobjectwithtextblocks.cpp:373)
-        by 0x82CFB9C: CodeDocument::setAttributesFromNode(QDomElement&) (codedocument.cpp:414)
-        by 0x82C3CAE: ClassifierCodeDocument::setAttributesFromNode(QDomElement&) (classifiercodedocument.cpp:595)
-        by 0x82C4388: ClassifierCodeDocument::loadFromXMI(QDomElement&) (classifiercodedocument.cpp:687)
-        by 0x82D372E: CodeGenerator::loadFromXMI(QDomElement&) (codegenerator.cpp:205)
-        by 0x8356381: UMLDoc::loadExtensionsFromXMI(QDomNode&) (umldoc.cpp:1774)
-        by 0x8353F16: UMLDoc::loadFromXMI(QIODevice&, short) (umldoc.cpp:1480)
-        by 0x834DBB6: UMLDoc::openDocument(KUrl const&, char const*) (umldoc.cpp:475)
-        by 0x833F0DE: UMLApp::openDocumentFile(KUrl const&) (uml.cpp:642)
-        by 0x83098D9: initDocument(KCmdLineArgs*) (main.cpp:125)
-        by 0x830974D: main (main.cpp:101)
-      Address 0x66539DC is 12 bytes inside a block of size 68 free'd
-        at 0x401D567: operator delete(void*) (vg_replace_malloc.c:244)
-        by 0x8204CA2: CPPSourceCodeAccessorMethod::~CPPSourceCodeAccessorMethod() (cppsourcecodeaccessormethod.cpp:51)
-        by 0x82D6C6D: CodeGenObjectWithTextBlocks::resetTextBlocks() (codegenobjectwithtextblocks.cpp:333)
-        by 0x82D6001: CodeGenObjectWithTextBlocks::~CodeGenObjectWithTextBlocks() (codegenobjectwithtextblocks.cpp:42)
-        by 0x82F272A: HierarchicalCodeBlock::~HierarchicalCodeBlock() (hierarchicalcodeblock.cpp:41)
-        by 0x82D6C6D: CodeGenObjectWithTextBlocks::resetTextBlocks() (codegenobjectwithtextblocks.cpp:333)
-        by 0x82CF294: CodeDocument::resetTextBlocks() (codedocument.cpp:330)
-        by 0x82026EC: CPPSourceCodeDocument::resetTextBlocks() (cppsourcecodedocument.cpp:111)
-        by 0x82D6DE7: CodeGenObjectWithTextBlocks::setAttributesFromNode(QDomElement&) (codegenobjectwithtextblocks.cpp:370)
-        by 0x82CFB9C: CodeDocument::setAttributesFromNode(QDomElement&) (codedocument.cpp:414)
-        by 0x82C3CAE: ClassifierCodeDocument::setAttributesFromNode(QDomElement&) (classifiercodedocument.cpp:595)
-        by 0x82C4388: ClassifierCodeDocument::loadFromXMI(QDomElement&) (classifiercodedocument.cpp:687)
-        by 0x82D372E: CodeGenerator::loadFromXMI(QDomElement&) (codegenerator.cpp:205)
-        by 0x8356381: UMLDoc::loadExtensionsFromXMI(QDomNode&) (umldoc.cpp:1774)
-        by 0x8353F16: UMLDoc::loadFromXMI(QIODevice&, short) (umldoc.cpp:1480)
-        by 0x834DBB6: UMLDoc::openDocument(KUrl const&, char const*) (umldoc.cpp:475)
-        by 0x833F0DE: UMLApp::openDocumentFile(KUrl const&) (uml.cpp:642)
-        by 0x83098D9: initDocument(KCmdLineArgs*) (main.cpp:125)
-        by 0x830974D: main (main.cpp:101)
+     **************  else crash happens on loading an XMI file
      *************/
 
     m_textBlockTagMap.clear();

umbrello/codegenwizard/codegenstatuspage.cpp
@@ -211,7 +211,7 @@ void CodeGenStatusPage::loggerClear()
 void CodeGenStatusPage::loggerExport()
 {
     const QString caption = i18n("Umbrello Code Generation - Logger Export");
-    QString fileName = KFileDialog::getSaveFileName(KUrl(), QString(), 0, caption);
+    QString fileName = KFileDialog::getSaveFileName(QUrl(), QString(), 0, caption);
     if (!fileName.isEmpty()) {
         QFile file(fileName);
         if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {

umbrello/codeimpwizard/codeimpstatuspage.cpp
@@ -327,7 +327,7 @@ void CodeImpStatusPage::loggerClear()
 void CodeImpStatusPage::loggerExport()
 {
     const QString caption = i18n("Umbrello Code Import - Logger Export");
-    QString fileName = KFileDialog::getSaveFileName(KUrl(), QString(), 0, caption);
+    QString fileName = KFileDialog::getSaveFileName(QUrl(), QString(), 0, caption);
     if (!fileName.isEmpty()) {
         QFile file(fileName);
         if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {

umbrello/dialogs/umlfiledialog.cpp
@@ -21,7 +21,7 @@
 
 static QStringList mime2KdeFilter(const QStringList &mimeTypes, QString *allExtensions = 0)
 {
-    const KUrl emptyUrl;
+    const QUrl emptyUrl;
     QStringList kdeFilter;
     QStringList allExt;
     QMimeDatabase db;
@@ -46,7 +46,7 @@ static QStringList mime2KdeFilter(const QStringList &mimeTypes, QString *allExte
 }
 
 
-UMLFileDialog::UMLFileDialog(const KUrl &startDir, const QString &filter, QWidget *parent, QWidget *widget)
+UMLFileDialog::UMLFileDialog(const QUrl &startDir, const QString &filter, QWidget *parent, QWidget *widget)
   : m_dialog(new KFileDialog(startDir, filter, parent, widget))
 {
 }
@@ -73,12 +73,12 @@ void UMLFileDialog::setOperationMode(KFileDialog::OperationMode mode)
     m_dialog->setOperationMode(mode);
 }
 
-void UMLFileDialog::setUrl(const KUrl &url, bool clearforward)
+void UMLFileDialog::setUrl(const QUrl &url, bool clearforward)
 {
     m_dialog->setUrl(url, clearforward);
 }
 
-KUrl UMLFileDialog::selectedUrl()
+QUrl UMLFileDialog::selectedUrl()
 {
     return m_dialog->selectedUrl();
 }
@@ -104,7 +104,7 @@ void UMLFileDialog::setMimeFilter(const QStringList &types, const QString &defau
  */
 QString UMLFileDialog::currentMimeFilter()
 {
-    KUrl url = m_dialog->selectedUrl();
+    QUrl url = m_dialog->selectedUrl();
     QFileInfo fi(url.toLocalFile());
     return UMLViewImageExporterModel::imageTypeToMimeType(fi.suffix());
 }

umbrello/dialogs/umlfiledialog.h
@@ -12,7 +12,8 @@
 #define UMLFILEDIALOG_H
 
 #include <kfiledialog.h>
-#include <kurl.h>
+
+#include <QUrl>
 
 /**
  * Wrapper class for KFileDialog supporting graphviz *.dot files.
@@ -27,14 +28,14 @@ class UMLFileDialog : public QObject
 {
     Q_OBJECT
 public:
-    UMLFileDialog(const KUrl &startDir, const QString &filter, QWidget *parent, QWidget *widget=0);
+    UMLFileDialog(const QUrl &startDir, const QString &filter, QWidget *parent, QWidget *widget=0);
     ~UMLFileDialog();
 
     int exec();
     void setCaption(const QString &caption);
     void setOperationMode(KFileDialog::OperationMode mode);
-    void setUrl(const KUrl &url, bool clearforward=true);
-    KUrl selectedUrl();
+    void setUrl(const QUrl &url, bool clearforward=true);
+    QUrl selectedUrl();
     void setSelection(const QString &name);
     void setMimeFilter(const QStringList &types, const QString &defaultType=QString());
     QString currentMimeFilter();

umbrello/folder.cpp
@@ -324,7 +324,8 @@ void UMLFolder::saveToXMI(QDomDocument& qDoc, QDomElement& qElement)
     // See if we can create the external file.
     // If not then internalize the folder.
     UMLDoc *umldoc = UMLApp::app()->document();
-    QString fileName = umldoc->url().directory() + QLatin1Char('/') + m_folderFile;
+    QUrl directory = umldoc->url().adjusted(QUrl::RemoveFilename);
+    QString fileName = directory.path() + m_folderFile;
     QFile file(fileName);
     if (!file.open(QIODevice::WriteOnly)) {
         uError() << m_folderFile << QLatin1String(": ")
@@ -480,7 +481,8 @@ bool UMLFolder::load(QDomElement& element)
                     if (!loadDiagramsFromXMI(diagramNode))
                         totalSuccess = false;
                 } else if (xtag == QLatin1String("external_file")) {
-                    const QString rootDir(umldoc->url().directory());
+                    QUrl directory = umldoc->url().adjusted(QUrl::RemoveFilename);
+                    const QString rootDir(directory.path());
                     QString fileName = el.attribute(QLatin1String("name"));
                     const QString path(rootDir + QLatin1Char('/') + fileName);
                     if (loadFolderFile(path))

umbrello/main.cpp
@@ -139,7 +139,7 @@ void initDocument(KCmdLineArgs *args)
         bool last = UmbrelloSettings::loadlast();
         QString file = UmbrelloSettings::lastFile();
         if(last && !file.isEmpty()) {
-            UMLApp::app()->openDocumentFile(KUrl(file));
+            UMLApp::app()->openDocumentFile(QUrl(file));
         } else {
             UMLApp::app()->newDocument();
         }
@@ -153,17 +153,17 @@ void exportAllViews(KCmdLineArgs *args, const QStringList &exportOpt)
 
     // export to the specified directory, or the directory where the file is saved
     // if no directory was specified
-    KUrl directory;
+    QUrl directory;
     QStringList directoryOpt = args->getOptionList("directory");
     if (directoryOpt.size() > 0) {
         directory = KCmdLineArgs::makeURL(directoryOpt.last().toLocal8Bit());
     } else {
-        directory = KUrl(UMLApp::app()->document()->url().directory());
+        directory = UMLApp::app()->document()->url().adjusted(QUrl::RemoveFilename);
     }
 
     bool useFolders = args->isSet("use-folders");
 
-    uDebug() << "directory: " << directory.prettyUrl();
+    uDebug() << "directory: " << directory.toDisplayString();
 
     // the event is posted so when the Qt loop begins it's processed. UMLApp process this event executing
     // the method it provides for exporting the views. Once all the views were exported, a quit event

commit 2970e2c9605fb7d9d9bc1c43e2c56159bd536623

Fix regression not using QUrl::fromLocalFile() introduced with commit b3fa5a7d.

umbrello/docgenerators/docbookgenerator.cpp
@@ -127,7 +127,7 @@ void DocbookGenerator::slotDocbookGenerationFinished(const QString& tmpFileName)
     url.addPath(fileName);
 #endif
 #if QT_VERSION >= 0x050000
-    KIO::Job* job = KIO::file_copy(QUrl(tmpFileName), url, -1, KIO::Overwrite | KIO::HideProgressInfo);
+    KIO::Job* job = KIO::file_copy(QUrl::fromLocalFile(tmpFileName), url, -1, KIO::Overwrite | KIO::HideProgressInfo);
     KJobWidgets::setWindow(job, (QWidget*)UMLApp::app());
     job->exec();
     if (!job->error()) {