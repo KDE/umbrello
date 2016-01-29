dd45e245990253af19988268c3a599cad91d8719
Ralf Habacker <ralf.habacker@freenet.de>
2016-01-30 00:13:51 +0100

    Add more c++ import testcases for parsing comments.
    CCBUG:358655

---
+++ test/import/cxx/comments-class-enums.h
@@ -0,0 +1,16 @@
+
+// class comments_class_enum_test
+class comments_class_enum_test {
+public:
+    // enim test c++ style single line comment
+    enum test1 { A, B, C};
+
+    /*
+     * enum test2
+     * c-style multi line comment
+     */
+    enum test2 { A, B, C};
+
+    /* enum test3 c-style single lime comment */
+    enum test3 { A, B, C};
+};
