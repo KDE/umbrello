dd6f93f468825c22827d71319a55d9c894793849
Ralf Habacker <ralf.habacker@freenet.de>
2016-03-15 09:07:46 +0100

    Fix bug showing asymmetric arrow for associations with type 'Dependency'.
    Arrows are always displayed solid.
    
    BUG:360531
    FIXED-IN:2.18.80 (KDE Applications 16.03.80)

--- umbrello/umlwidgets/associationline.cpp
+++ umbrello/umlwidgets/associationline.cpp
@@ -1409,8 +1409,7 @@ void Symbol::setPen(const QPen& pen)
 {
     prepareGeometryChange();
     m_pen = pen;
-    if (m_symbolType == ClosedArrow)
-        m_pen.setStyle(Qt::SolidLine);
+    m_pen.setStyle(Qt::SolidLine);
 }
 
 /**
