a4c1fab53da43408dd320c911129604656fdfb02
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-05-27 16:41:30 -0300

    Fix method and layout
    -Edit the constructor to receive more parameters and init
    variables
    -Edit widget name
    -Fix addToLayout to add the label and radioButtons separated
    -Fix apply method
    -Fix layout issues on init method.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- umbrello/dialogs/widgets/umltypequalifiers.cpp
+++ umbrello/dialogs/widgets/umltypequalifiers.cpp
@@ -1,39 +1,50 @@
 #include "umltypequalifiers.h"
+#include "umlobject.h"
+#include "uml.h"
 
 #include <KLocalizedString>
 
-UmlTypeQualifiers::UmlTypeQualifiers(QWidget *parent) : QWidget(parent)
+UmlTypeQualifiers::UmlTypeQualifiers(UMLObject *o,QWidget *parent) : QWidget(parent)
+  , m_object(o)
+  , m_role(Uml::RoleType::A)
 {
+    Q_ASSERT(o);
     m_texts[Uml::TypeQualifiers::Const] = i18nc("const type", "&Const");
     m_texts[Uml::TypeQualifiers::Volatile] = i18nc("volatile type", "&Volatile");
     m_texts[Uml::TypeQualifiers::Mutable] = i18nc("mutable type", "&Mutable");
-    init(i18n("Type Qualifiers"));
+    init(i18n("Type Qualifiers:"));
 }
 
-void UmlTypeQualifiers::addToLayout(QVBoxLayout *layout)
+void UmlTypeQualifiers::addToLayout(QGridLayout *layout, int row)
 {
-    layout->addWidget(m_box);
+    layout->addWidget(m_label, row, 0);
+    layout->addWidget(this, row, 1);
+}
+
+void UmlTypeQualifiers::apply()
+{
+    for(ButtonMap::const_iterator i = m_buttons.constBegin(); i != m_buttons.constEnd(); ++i) {
+        if (i.value()->isChecked()) {
+            if (m_object)
+                m_object->setQualifiers(i.key());
+        }
+    }
 }
 
 void UmlTypeQualifiers::init(const QString &title)
 {
-    auto *layout = new QHBoxLayout;
-    layout->setContentsMargins(0,0,0,0);
-    m_box = new QGroupBox(title, this);
-
-    QHBoxLayout* boxlayout = new QHBoxLayout(m_box);
-    int margin = fontMetrics().height();
-    boxlayout->setMargin(margin);
+    m_box = new QHBoxLayout;
+    m_box->setContentsMargins(0,0,0,0);
+    m_label = new QLabel(title);
 
     QList<Uml::TypeQualifiers::Enum> list;
     list << Uml::TypeQualifiers::Const << Uml::TypeQualifiers::Volatile << Uml::TypeQualifiers::Mutable;
 
     for(QList<Uml::TypeQualifiers::Enum>::const_iterator i = list.constBegin(); i != list.constEnd(); ++i){
         Uml::TypeQualifiers::Enum key = *i;
-        auto *button = new QRadioButton(m_texts[key], m_box);
+        auto *button = new QRadioButton(m_texts[key]);
         m_buttons[key] = button;
-        boxlayout->addWidget(button);
+        m_box->addWidget(button);
     }
-    layout->addWidget(m_box);
-    setLayout(layout);
+    setLayout(m_box);
 }
