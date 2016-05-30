6ee42e0fb1aeea06185eef2b75212bdaad2768cc
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-05-30 15:53:49 -0300

    Add new TypeQualifiers
    Add the 'none' and 'const-volatile' qualifiers, to the
    description and to the list of widgets.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- umbrello/dialogs/widgets/umltypequalifierswidget.cpp
+++ umbrello/dialogs/widgets/umltypequalifierswidget.cpp
@@ -12,6 +12,8 @@ UmlTypeQualifiersWidget::UmlTypeQualifiersWidget(UMLObject *o,QWidget *parent) :
     m_texts[Uml::TypeQualifiers::Const] = i18nc("const type", "&Const");
     m_texts[Uml::TypeQualifiers::Volatile] = i18nc("volatile type", "&Volatile");
     m_texts[Uml::TypeQualifiers::Mutable] = i18nc("mutable type", "&Mutable");
+    m_texts[Uml::TypeQualifiers::ConstVolatile] = i18nc("const volatile type", "&Const Volatile");
+    m_texts[Uml::TypeQualifiers::None] = i18nc("none", "&None");
     init(i18n("Type Qualifiers:"));
 }
 
@@ -38,7 +40,7 @@ void UmlTypeQualifiersWidget::init(const QString &title)
     m_label = new QLabel(title);
 
     QList<Uml::TypeQualifiers::Enum> list;
-    list << Uml::TypeQualifiers::Const << Uml::TypeQualifiers::Volatile << Uml::TypeQualifiers::Mutable;
+    list << Uml::TypeQualifiers::None << Uml::TypeQualifiers::Const << Uml::TypeQualifiers::Volatile << Uml::TypeQualifiers::Mutable << Uml::TypeQualifiers::ConstVolatile;
 
     for(QList<Uml::TypeQualifiers::Enum>::const_iterator i = list.constBegin(); i != list.constEnd(); ++i){
         Uml::TypeQualifiers::Enum key = *i;
