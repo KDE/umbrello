214e4bfde170f5c8f767157996cb6df72f3d651f
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-06-12 23:10:17 +0200

    Major changes
    Created a method to receive the UMLClassifierListItem
    that was removed of the constructor
    Removed addToLayout method
    Fix layout issues
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- umbrello/dialogs/widgets/umltypequalifierswidget.cpp
+++ umbrello/dialogs/widgets/umltypequalifierswidget.cpp
@@ -4,23 +4,16 @@
 
 #include <KLocalizedString>
 
-UmlTypeQualifiersWidget::UmlTypeQualifiersWidget(UMLClassifierListItem *o,QWidget *parent)
+UmlTypeQualifiersWidget::UmlTypeQualifiersWidget(QWidget *parent)
   : QWidget(parent)
-  , m_qualifier(o)
 {
-    Q_ASSERT(o);
     m_texts[Uml::TypeQualifiers::Const] = i18nc("const type", "&Const");
     m_texts[Uml::TypeQualifiers::Volatile] = i18nc("volatile type", "&Volatile");
     m_texts[Uml::TypeQualifiers::Mutable] = i18nc("mutable type", "&Mutable");
     m_texts[Uml::TypeQualifiers::ConstVolatile] = i18nc("const volatile type", "&Const Volatile");
     m_texts[Uml::TypeQualifiers::None] = i18nc("none", "&None");
     init(i18n("Type Qualifiers:"));
-}
 
-void UmlTypeQualifiersWidget::addToLayout(QGridLayout *layout, int row)
-{
-    layout->addWidget(m_label, row, 0);
-    layout->addWidget(this, row, 1);
 }
 
 void UmlTypeQualifiersWidget::apply()
@@ -33,20 +26,27 @@ void UmlTypeQualifiersWidget::apply()
     }
 }
 
+void UmlTypeQualifiersWidget::setUMLClassifierItem(UMLClassifierListItem *o)
+{
+    Q_ASSERT(o);
+    m_qualifier = o;
+}
+
 void UmlTypeQualifiersWidget::init(const QString &title)
 {
-    m_box = new QHBoxLayout;
+    m_box = new QGridLayout;
     m_box->setContentsMargins(0,0,0,0);
     m_label = new QLabel(title);
+    m_box->addWidget(m_label,0,0);
 
     QList<Uml::TypeQualifiers::Enum> list;
     list << Uml::TypeQualifiers::None << Uml::TypeQualifiers::Const << Uml::TypeQualifiers::Volatile << Uml::TypeQualifiers::Mutable << Uml::TypeQualifiers::ConstVolatile;
-
-    for(QList<Uml::TypeQualifiers::Enum>::const_iterator i = list.constBegin(); i != list.constEnd(); ++i){
+    int cont=0;
+    for(QList<Uml::TypeQualifiers::Enum>::const_iterator i = list.constBegin(); i != list.constEnd(); ++i, ++cont){
         Uml::TypeQualifiers::Enum key = *i;
         auto *button = new QRadioButton(m_texts[key]);
         m_buttons[key] = button;
-        m_box->addWidget(button);
+        m_box->addWidget(button,1,cont);
     }
     setLayout(m_box);
 }
--- umbrello/dialogs/widgets/umltypequalifierswidget.h
+++ umbrello/dialogs/widgets/umltypequalifierswidget.h
@@ -12,10 +12,10 @@ class UmlTypeQualifiersWidget : public QWidget
 {
     Q_OBJECT
 public:
-    explicit UmlTypeQualifiersWidget(UMLClassifierListItem *o, QWidget *parent = 0);
+    explicit UmlTypeQualifiersWidget(QWidget *parent = 0);
 
-    void addToLayout(QGridLayout *layout,int row);
     void apply();
+    void setUMLClassifierItem(UMLClassifierListItem *o);
 
     typedef QMap<Uml::TypeQualifiers::Enum,QString> TextMap;
     typedef QMap<Uml::TypeQualifiers::Enum,QRadioButton*> ButtonMap;
@@ -24,7 +24,7 @@ private:
     void init(const QString &title);
 
     TextMap m_texts;
-    QHBoxLayout *m_box;
+    QGridLayout *m_box;
     ButtonMap m_buttons;
     UMLClassifierListItem *m_qualifier;
     QLabel *m_label;
