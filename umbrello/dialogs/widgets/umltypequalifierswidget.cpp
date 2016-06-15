f8042d756b23a1d8e236a686caad88fdcd6d3ad8
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-06-15 20:18:06 +0200

    Fix enable/disable widgets
    We need to have some control of the widgets select here
    Add some connects to manage this.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- umbrello/dialogs/widgets/umltypequalifierswidget.cpp
+++ umbrello/dialogs/widgets/umltypequalifierswidget.cpp
@@ -10,6 +10,43 @@ UmlTypeQualifiersWidget::UmlTypeQualifiersWidget(QWidget *parent)
   ,ui(new Ui::UMLTypeQualifiersWidget)
 {
     ui->setupUi(this);
+    //Using some connects with lambda to control the widgets
+    connect(ui->rb_none, &QRadioButton::clicked, [=]{
+        if(ui->rb_none->isChecked()){
+            ui->rb_mutable->setEnabled(true);
+            ui->ck_const->setEnabled(true);
+            ui->ck_volatile->setEnabled(true);
+        }
+    });
+
+    connect(ui->rb_mutable, &QRadioButton::clicked, [=]{
+        if(ui->rb_mutable->isChecked()){
+            ui->ck_const->setDisabled(true);
+            ui->ck_volatile->setDisabled(true);
+        }
+    });
+    connect(ui->ck_const, &QCheckBox::clicked, [=]{
+        if(ui->ck_const->isChecked() || ui->ck_volatile->isChecked()){
+            ui->rb_mutable->setDisabled(true);
+            ui->rb_none->setDisabled(true);
+            ui->rb_none->setChecked(false);
+        }
+        else{
+            ui->rb_mutable->setDisabled(false);
+            ui->rb_none->setDisabled(false);
+        }
+    });
+    connect(ui->ck_volatile, &QCheckBox::clicked, [=]{
+       if(ui->ck_volatile->isChecked() || ui->ck_const->isChecked()){
+           ui->rb_mutable->setDisabled(true);
+           ui->rb_none->setDisabled(true);
+           ui->rb_none->setChecked(false);
+       }
+       else{
+           ui->rb_mutable->setDisabled(false);
+           ui->rb_none->setDisabled(false);
+       }
+    });
 }
 
 void UmlTypeQualifiersWidget::apply()
