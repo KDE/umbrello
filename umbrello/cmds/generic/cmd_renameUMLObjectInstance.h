2a6c144c10105bdab2bc6dba83dd692b8584f7e4
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-08-12 21:21:52 -0300

    New methods
    - Constructor to add to log the change of the name
    - Redo and Undo to handle instance name changes
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- umbrello/cmds/generic/cmd_renameUMLObjectInstance.h
+++ umbrello/cmds/generic/cmd_renameUMLObjectInstance.h
@@ -1,11 +1,36 @@
-#ifndef CMDRENAMEUMLOBJECTINSTANCE_H
-#define CMDRENAMEUMLOBJECTINSTANCE_H
+/***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   copyright (C) 2002-2016                                              *
+ *   Umbrello UML Modeller Authors <umbrello-devel@kde.org>                *
+ ***************************************************************************/
 
+#ifndef CMD_RENAME_UMLOBJECTINSTANCE_H
+#define CMD_RENAME_UMLOBJECTINSTANCE_H
 
-class CmdRenameUMLObjectInstance
+#include <QUndoCommand>
+
+class UMLObject;
+
+namespace Uml
 {
-public:
-    CmdRenameUMLObjectInstance();
-};
+    class CmdRenameUMLObjectInstance : public QUndoCommand
+    {
+        public:
+            CmdRenameUMLObjectInstance(UMLObject* o, const QString& name);
+            ~CmdRenameUMLObjectInstance();
 
-#endif // CMDRENAMEUMLOBJECTINSTANCE_H
+            void redo();
+            void undo();
+
+        private:
+            UMLObject*  m_obj;
+            QString     m_name;
+            QString     m_oldname;
+    };
+}
+
+#endif
