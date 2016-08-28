d7fcbdb674f80330befc527140e1cfa7d0ae3571
Ralf Habacker <ralf.habacker@freenet.de>
2016-08-29 01:00:17 +0200

    Revert "Fix compile issue on Ubuntu 14.04.4 (trusty)."
    This reverts commit d07b449805b383e366d3f813e2462280cdc61596, which
    breaks the whole idea of this patch.

--- umbrello/main.cpp
+++ umbrello/main.cpp
@@ -163,7 +163,6 @@ int main(int argc, char *argv[])
     args->process(app);
 
     aboutData.processCommandLine(args);
-    Q_INIT_RESOURCE(icons);
 #else
     KCmdLineArgs::init(argc, argv, &aboutData);
     KCmdLineOptions options;
@@ -176,8 +175,8 @@ int main(int argc, char *argv[])
     options.add("use-folders", ki18n("keep the tree structure used to store the views in the document in the target directory"));
     KCmdLineArgs::addCmdLineOptions(options); // Add our own options.
     KApplication app;
-    Q_INIT_RESOURCE_EXTERN(icons);
 #endif
+    Q_INIT_RESOURCE(icons);
 
     QPointer<UMLApp> uml;
     if (app.isSessionRestored()) {
