0b9b23481cc5d73e5dae86f257f66e2b56d22708
Ralf Habacker <ralf.habacker@freenet.de>
2016-09-28 11:34:25 +0200

    Fix coverity check CID 155784: Uninitialized pointer field (UNINIT_CTOR)
    Non-static class member viewObjectsWindow is not initialized in this
    constructor nor in any functions that it calls.
    
    CCBUG:340646

--- umbrello/umlappprivate.h
+++ umbrello/umlappprivate.h
@@ -67,6 +67,7 @@ public:
       : parent(_parent),
         findDialog(_parent),
         viewDiagramsWindow(0),
+        viewObjectsWindow(0),
         viewStereotypesWindow(0),
         diagramsWindow(0),
         objectsWindow(0),
