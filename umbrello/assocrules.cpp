1e48acba53a0fc1760788d395edaca0b90e28cec
Ralf Habacker <ralf.habacker@freenet.de>
2017-03-13 14:37:26 +0100

    Fix 'Java enum association fails'.
    To fix this issue an associaton rule to allow associations between
    enums for java language has been added.
    
    BUG:131591
    FIXED-IN: 2.21.80 (KDE Applications 17.03.80)

--- umbrello/assocrules.cpp
+++ umbrello/assocrules.cpp
@@ -415,6 +415,7 @@ AssocRules::Assoc_Rule AssocRules::m_AssocRules[] = {
     { All, Uml::AssociationType::Association,      WidgetBase::wt_Interface,  WidgetBase::wt_Artifact,    true,   false,  false,  false },
     { All, Uml::AssociationType::Association,      WidgetBase::wt_Node,       WidgetBase::wt_Node,        true,   false,  false,  false },
     { All, Uml::AssociationType::Association,      WidgetBase::wt_Node,       WidgetBase::wt_Node,        true,   false,  false,  false },
+    {Java, Uml::AssociationType::Association,      WidgetBase::wt_Enum,       WidgetBase::wt_Enum,        true,   false,  false,   false },
     { All, Uml::AssociationType::UniAssociation,   WidgetBase::wt_Class,      WidgetBase::wt_Class,       true,   true,   true,   true  },
     { All, Uml::AssociationType::UniAssociation,   WidgetBase::wt_Object,     WidgetBase::wt_Object,      true,   true,   true,   true  },
     { All, Uml::AssociationType::UniAssociation,   WidgetBase::wt_Interface,  WidgetBase::wt_Interface,   true,   true,   true,   true  },
@@ -532,6 +533,8 @@ int AssocRules::m_nNumRules = sizeof(m_AssocRules) / sizeof(AssocRules::Assoc_Ru
 bool AssocRules::Assoc_Rule::isValid() const
 {
     if (language == All)
+        return true;
+    else if (language == Java && UMLApp::app()->activeLanguage() == Uml::ProgrammingLanguage::Java)
         return true;
     else
         return false;
