8a746c92aa58d85c579e56a4e6edcbccd60f8531
Ralf Habacker <ralf.habacker@freenet.de>
2018-01-03 13:32:24 +0100

    Avoid filename conflicts with original kdev-php package
    CCMAIL:i18n-infrastructure@kde.org

--- lib/kdev5-php/Messages.sh
+++ lib/kdev5-php/Messages.sh
@@ -3,5 +3,5 @@ files=`find . -name \*.rc -o -name \*.ui -o -name \*.kcfg`
 if [ "x$files" != "x" ]; then
 	$EXTRACTRC $files >> rc.cpp
 fi
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kdevphp.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/umbrello_kdevphp.pot
 rm -f rc.cpp
