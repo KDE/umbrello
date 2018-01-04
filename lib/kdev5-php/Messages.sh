6f69ac94b6f494ef4bf2e4e25c460895dcc72d28
Ralf Habacker <ralf.habacker@freenet.de>
2018-01-04 10:55:03 +0100

    Fix name clash between KF5 and KDE4 pot file
    CCMAIL:i18n-infrastructure@kde.org

--- lib/kdev5-php/Messages.sh
+++ lib/kdev5-php/Messages.sh
@@ -3,5 +3,5 @@ files=`find . -name \*.rc -o -name \*.ui -o -name \*.kcfg`
 if [ "x$files" != "x" ]; then
 	$EXTRACTRC $files >> rc.cpp
 fi
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/umbrello_kdevphp.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/umbrello_kdevphp5.pot
 rm -f rc.cpp
