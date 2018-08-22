2d3369b2252b1f6255f9a4afc0d607a5c067c858
Ralf Habacker <ralf.habacker@freenet.de>
2018-08-22 13:46:56 +0200

    Adding a C++ test case with multiple template parameters
    CCBUG:397664

--- test/import/cxx/templates.h
+++ test/import/cxx/templates.h
@@ -11,3 +11,17 @@ public:
   t2() {}
   ~t2() {}
 };
+
+template <class T, class U>
+class t3 {
+public:
+  t3() {}
+  ~t3() {}
+};
+
+template <class T, class U>
+class t4: public t3<T, U> {
+public:
+  t4() {}
+  ~t4() {}
+};
