7789aa8f968b7c2b22842e8c898ba9f3ad08d987
Ralf Habacker <ralf.habacker@freenet.de>
2019-06-27 12:24:56 +0200

    Fix cmake error 'cannot parse the expression: " + 1"'

--- cmake/modules/ECMKDE4Macros.cmake
+++ cmake/modules/ECMKDE4Macros.cmake
@@ -96,6 +96,9 @@ endmacro()
 
 macro(find_package package)
     # avoid recursive loops
+    if(NOT DEFEINED in_find_package)
+        set(in_find_package 0)
+    endif()
     math(EXPR in_find_package "${in_find_package} + 1")
     #message("-- ${ARGV} --")
     if("${package}" MATCHES "^(Qt5)")
