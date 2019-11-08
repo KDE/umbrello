2f8fbf2032882b57c0bc522d7b2f9511cf7d5c70
Ralf Habacker <ralf.habacker@freenet.de>
2019-11-08 02:03:58 +0100

    Add license header to diagramproxywidget.cpp|h

--- umbrello/umlwidgets/diagramproxywidget.h
+++ umbrello/umlwidgets/diagramproxywidget.h
@@ -1,3 +1,13 @@
+/***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   copyright (C) 2019                                                    *
+ *   Umbrello UML Modeller Authors <umbrello-devel@kde.org>                *
+ ***************************************************************************/
+
 #ifndef DIAGRAMPROXYWIDGET_H
 #define DIAGRAMPROXYWIDGET_H
 
