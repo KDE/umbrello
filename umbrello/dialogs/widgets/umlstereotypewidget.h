7cf3550f95be94904a67389325d15cae823878fa
Ralf Habacker <ralf.habacker@freenet.de>
2019-11-27 11:52:59 +0100

    Let class UMLStereotypeWidget use ComboBoxWidgetBase as base class
    Followup for commit f0b38d1051fcf684fc4d4fae59e2b43b7ae083d7.

--- umbrello/dialogs/widgets/umlstereotypewidget.h
+++ umbrello/dialogs/widgets/umlstereotypewidget.h
@@ -4,39 +4,29 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   copyright (C) 2002-2014                                               *
+ *   copyright (C) 2002-2014,2019                                          *
  *   Umbrello UML Modeller Authors <umbrello-devel@kde.org>                *
  ***************************************************************************/
 
 #ifndef UMLSTEREOTYPEWIDGET_H
 #define UMLSTEREOTYPEWIDGET_H
 
-#include <QWidget>
+#include "comboboxwidgetbase.h"
 
 class UMLObject;
 class UMLStereotype;
 
-class KComboBox;
-
-class QGridLayout;
-class QLabel;
-
-class UMLStereotypeWidget : public QWidget
+class UMLStereotypeWidget : public ComboBoxWidgetBase
 {
 public:
     explicit UMLStereotypeWidget(UMLObject *object, QWidget *parent=0);
-    ~UMLStereotypeWidget();
 
     void setEditable(bool state);
-    void addToLayout(QGridLayout *layout, int row);
     void apply();
 
 protected:
-    QLabel *m_label;
-    KComboBox *m_comboBox;
     UMLObject *m_object;
 
-    void init();
     void insertItems(UMLStereotype *type);
 };
 
