f749285a07c8111f622f878d4cae210d426565c1
Ralf Habacker <ralf.habacker@freenet.de>
2019-12-22 23:38:12 +0000

    fix url

--- maintainer/ci-build.sh
+++ maintainer/ci-build.sh
@@ -10,7 +10,7 @@ set
 #------------------------
 # trigger obs windows build 
 token=CBbFTso398hhg4WHESdUPE1n
-curl http://173.212.229.51/cgi-bin/run-obs-build?repo=${CI_PROJECT_URL}.git&revision=$CI_COMMIT_SHORT_SHA&job=$CI_JOB_ID&apitoken=$token
+curl "http://173.212.229.51/cgi-bin/run-obs-build?repo=${CI_PROJECT_URL}.git&revision=$CI_COMMIT_SHORT_SHA&job=$CI_JOB_ID&apitoken=$token"
 
 
 #------------------------
