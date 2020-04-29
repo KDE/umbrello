799f803b00c0eae1ccfe193de8c3a70b23fc7290
Ralf Habacker <ralf.habacker@freenet.de>
2020-04-29 22:21:58 +0200

    CI: Add local build target

--- maintainer/ci-build.sh
+++ maintainer/ci-build.sh
@@ -4,19 +4,18 @@
 # dump env
 set
 
-#------------------------
-# trigger obs windows build 
-token=CBbFTso398hhg4WHESdUPE1n
-curl "http://173.212.229.51/cgi-bin/run-obs-build?repo=${CI_PROJECT_URL}.git&revision=$CI_COMMIT_SHORT_SHA&job=$CI_JOB_ID&apitoken=$token"
+case "$ci_variant" in
+    (remote)
+        # trigger obs windows build
+        token=CBbFTso398hhg4WHESdUPE1n
+        curl "http://173.212.229.51/cgi-bin/run-obs-build?repo=${CI_PROJECT_URL}.git&revision=$CI_COMMIT_SHORT_SHA&job=$CI_JOB_ID&apitoken=$token"
+        ;;
 
-
-#------------------------
-# build unix variant
-if test -n "$CI_UNIX"; then
-    # local build
-    zypper --non-interactive si umbrello
-    git clone https://invent.kde.org/kde/umbrello.git
-    mkdir -p umbrello-build && cd umbrello-build
-    cmake ../umbrello
-    make -j5
-fi
+    (local)
+        # local build
+        zypper --non-interactive si umbrello
+        mkdir -p build && cd build
+        cmake ..
+        make -j5
+        ;;
+esac
