133d1ab25ee7b6c87f2ffbeaaadaf6b68afc2399
l10n daemon script <scripty@kde.org>
2020-08-05 09:16:27 +0200

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- lib/kdev5-php/docs/kcm_kdevphpdocs.desktop.cmake
+++ lib/kdev5-php/docs/kcm_kdevphpdocs.desktop.cmake
@@ -12,8 +12,8 @@ X-KDE-CfgDlgHierarchy=DOCUMENTATION
 X-KDE-Weight=150
 
 Name=PHP Documentation
-Name[ca]=Documentació del PHP
-Name[ca@valencia]=Documentació del PHP
+Name[ca]=Documentació de PHP
+Name[ca@valencia]=Documentació de PHP
 Name[cs]=Dokumentace PHP
 Name[de]=PHP-Dokumentation
 Name[el]=PHP τεκμηρίωση
