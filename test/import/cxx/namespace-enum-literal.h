5892e7e14438d400f4df52e8b6c20f1633bee34e
Oliver Kellogg <okellogg@users.sourceforge.net>
2021-04-20 23:09:20 +0200

    test/import/cxx/namespace-enum-literal.h : Document semicolon after end of namespace.

--- test/import/cxx/namespace-enum-literal.h
+++ test/import/cxx/namespace-enum-literal.h
@@ -1,4 +1,4 @@
 namespace namespace_enum_literal {
     /* should display the following literals in tree view * A = 1 * B = 2 * C * D */
     enum test { A = 1, B = 2, C, D };
-};
+};  // This last semicolon should not be there but most compilers accept it.
