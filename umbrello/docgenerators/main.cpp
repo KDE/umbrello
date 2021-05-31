9290673108717d104246fe5cabc28783dac653c9
Luigi Toscano <luigi.toscano@tiscali.it>
2021-05-31 22:32:31 +0200

    Fix user visible copyright string (no SPDX) (2)
    CCMAIL: alexander.lohnau@gmx.de

--- umbrello/docgenerators/main.cpp
+++ umbrello/docgenerators/main.cpp
@@ -45,7 +45,7 @@ int main(int argc, char *argv[])
 
   KAboutData aboutData("umbodoc", 0, ki18n("Umbrello UML Modeller autonomous code generator"),
                         umbrelloVersion(), ki18n(description), KAboutData::License_GPL,
-                        ki18n("(c) 2006 Gael de Chalendar (aka Kleag), SPDX-FileCopyrightText: 2002-2006 Umbrello UML Modeller Authors "), KLocalizedString(),
+                        ki18n("(c) 2006 Gael de Chalendar (aka Kleag), (c) 2002-2006 Umbrello UML Modeller Authors"), KLocalizedString(),
                         "http://umbrello.kde.org/");
   aboutData.addAuthor(ki18n("Gael de Chalendar (aka Kleag)"),KLocalizedString(), "kleag@free.fr");
   aboutData.addAuthor(ki18n("Umbrello UML Modeller Authors"), KLocalizedString(), "umbrello-devel@kde.org");
