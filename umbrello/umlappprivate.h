6aebf0aab934087c3caeabb5b765d07d0c374876
Oliver Kellogg <okellogg@users.sourceforge.net>
2022-01-29 22:51:08 +0100

    umbrello/umlappprivate.h : Remove unnecessary #include "debug_utils.h". In future this file shall only be #included on cpp file level.

--- umbrello/umlappprivate.h
+++ umbrello/umlappprivate.h
@@ -1,6 +1,6 @@
 /*
     SPDX-License-Identifier: GPL-2.0-or-later
-    SPDX-FileCopyrightText: 2002-2021 Umbrello UML Modeller Authors <umbrello-devel@kde.org>
+    SPDX-FileCopyrightText: 2002-2022 Umbrello UML Modeller Authors <umbrello-devel@kde.org>
 */
 
 #ifndef UMLAPPPRIVATE_H
@@ -8,7 +8,6 @@
 
 // app includes
 #include "cmds.h"
-#include "debug_utils.h"
 #include "finddialog.h"
 #include "findresults.h"
 #include "uml.h"
