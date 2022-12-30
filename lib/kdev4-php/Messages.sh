5edff992f70db9f6eccfebace9140f21a90506a7
Andreas Sturmlechner <asturm@gentoo.org>
2022-12-30 20:58:42 +0100

    Drop obsolete bundled kdev4-php
    Signed-off-by: Andreas Sturmlechner <asturm@gentoo.org>

--- lib/kdev4-php/Messages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-files=`find . -name \*.rc -o -name \*.ui`
-if [ "x$files" != "x" ]; then
-	$EXTRACTRC $files >> rc.cpp
-fi
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/umbrello_kdevphp.pot
-rm -f rc.cpp
