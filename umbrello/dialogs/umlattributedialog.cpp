d5a9641797d68029856aa6f69da911d77423089e
Tomaz Canabrava <tcanabrava@kde.org>
2024-04-15 19:18:23 +0200

    Remove last parameter of KMessageBox::error

--- umbrello/dialogs/umlattributedialog.cpp
+++ umbrello/dialogs/umlattributedialog.cpp
@@ -120,7 +120,7 @@ bool UMLAttributeDialog::apply()
     QString name = m_pNameLE->text();
     if (name.isEmpty()) {
         KMessageBox::error(this, i18n("You have entered an invalid attribute name."),
-                           i18n("Attribute Name Invalid"), 0);
+                           i18n("Attribute Name Invalid"));
         m_pNameLE->setText(m_pAttribute->name());
         return false;
     }
@@ -128,7 +128,7 @@ bool UMLAttributeDialog::apply()
     UMLObject *o = pConcept ? pConcept->findChildObject(name) : 0;
     if (o && o != m_pAttribute) {
         KMessageBox::error(this, i18n("The attribute name you have chosen is already being used in this operation."),
-                           i18n("Attribute Name Not Unique"), 0);
+                           i18n("Attribute Name Not Unique"));
         m_pNameLE->setText(m_pAttribute->name());
         return false;
     }
