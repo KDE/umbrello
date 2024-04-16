b78aa0624cb690116c1900adf81f22e407aad6b7
Tomaz Canabrava <tcanabrava@kde.org>
2024-04-16 18:46:29 +0200

    WebEngine

--- umbrello/umlappprivate.h
+++ umbrello/umlappprivate.h
@@ -153,8 +153,6 @@ public Q_SLOTS:
         welcomeWindow->setObjectName(QStringLiteral("WelcomeDock"));
 
         auto *view = new QWebEngineView();
-        view->page()->setLinkDelegationPolicy(QWebPage::DelegateAllLinks);
-        view->setContextMenuPolicy(Qt::NoContextMenu);
         connect(view, SIGNAL(linkClicked(const QUrl)), this, SLOT(slotWelcomeWindowLinkClicked(const QUrl)));
         view->setHtml(html);
         view->show();
