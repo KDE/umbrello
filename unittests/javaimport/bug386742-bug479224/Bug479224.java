b120efd5b5913c90edb2f95ee5f54d512ae13656
Oliver Kellogg <olivermkellogg@gmail.com>
2025-04-19 08:23:45 +0200

    Start adding unittests/javaimport

---
+++ unittests/javaimport/bug386742-bug479224/Bug479224.java
@@ -0,0 +1,7 @@
+// https://bugs.kde.org/show_bug.cgi?id=479224
+// only added here for being able to select the other class (DerivedExtern.java)
+
+package apackage;
+
+class Bug479224 {
+}
---
+++ unittests/javaimport/bug386742-bug479224/DerivedExtern.java
@@ -0,0 +1,9 @@
+// https://bugs.kde.org/show_bug.cgi?id=386742
+
+package apackage;
+
+import org.kde.BaseClass;
+
+public class DerivedExtern extends BaseClass {
+     public DerivedExtern () {}
+}
