7edcb62dbd549e5019d4a281bee8e90ab5e102c5
Oliver Kellogg <olivermkellogg@gmail.com>
2025-04-19 08:50:02 +0200

    Move unittests/{csharp,java,sql}import content to test/import/

--- unittests/javaimport/bug386742-bug479224/Bug479224.java
+++
@@ -1,7 +0,0 @@
-// https://bugs.kde.org/show_bug.cgi?id=479224
-// only added here for being able to select the other class (DerivedExtern.java)
-
-package apackage;
-
-class Bug479224 {
-}
--- unittests/javaimport/bug386742-bug479224/DerivedExtern.java
+++
@@ -1,9 +0,0 @@
-// https://bugs.kde.org/show_bug.cgi?id=386742
-
-package apackage;
-
-import org.kde.BaseClass;
-
-public class DerivedExtern extends BaseClass {
-     public DerivedExtern () {}
-}
